3b3485e99e1987d5709a5e80dedda96984c97931
Craig Hawker <craig.hawker@gmail.com>
2020-02-26 11:51:53 +0000

    Removed packages

--- packages/MFiles.VAF.2.1.0.7/tools/init.ps1
+++
@@ -1,2 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-$project.Object.References | Where-Object { $_.Name -eq "Interop.MFilesAPI" } |  ForEach-Object { $_.EmbedInteropTypes = $false }
