aedd2a4611960729adfdd0761922de903dac5314
Craig Hawker <craig.hawker@gmail.com>
2020-02-27 11:20:16 +0000

    Permissions-related MFSearchBuilder extension methods. Unit tests for above.

---
+++ MFiles.VAF.Extensions.Tests/ExtensionMethods/MFSearchBuilderExtensionMethods/Permissions.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MFilesAPI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MFiles.VAF.Extensions.Tests.ExtensionMethods.MFSearchBuilderExtensionMethods
+{
+	[TestClass]
+	public class Permissions
+		: MFSearchBuilderExtensionMethodTestBase
+	{
+		/// <summary>
+		/// Tests that calling
+		/// <see cref="MFSearchBuilderExtensionMethods.UserHasPermissionTo"/>
+		/// adds a search condition.
+		/// </summary>
+		[TestMethod]
+		public void AddsSearchCondition()
+		{
+			// Create the search builder.
+			var mfSearchBuilder = this.GetSearchBuilder();
+
+			// Ensure it has no items in the collection.
+			Assert.AreEqual(0, mfSearchBuilder.Conditions.Count);
+
+			// Add the search condition for whether the user has the required permissions.
+			mfSearchBuilder.UserHasPermissionTo(23, MFPermissionsExpressionType.MFVisibleTo);
+
+			// Ensure that there is one item in the collection.
+			Assert.AreEqual(1, mfSearchBuilder.Conditions.Count);
+		}
+		
+		/// <summary>
+		/// Tests that calling
+		/// <see cref="MFSearchBuilderExtensionMethods.UserHasPermissionTo"/>
+		/// adds a valid search condition.
+		/// </summary>
+		[TestMethod]
+		[DataRow(25, MFPermissionsExpressionType.MFVisibleTo)]
+		[DataRow(255, MFPermissionsExpressionType.MFEditableBy)]
+		[DataRow(250, MFPermissionsExpressionType.MFDeletableBy)]
+		[DataRow(21, MFPermissionsExpressionType.MFPermissionsChangeableBy)]
+		[DataRow(20, MFPermissionsExpressionType.MFFullControlBy)]
+		[DataRow(50, MFPermissionsExpressionType.MFObjectsAttachableToThisItemBy)]
+		public void SearchConditionIsCorrect
+		(
+			int userId,
+			MFPermissionsExpressionType permission
+		)
+		{
+			// Create the search builder.
+			var mfSearchBuilder = this.GetSearchBuilder();
+			
+			// Add the search condition for whether the user has the required permissions.
+			mfSearchBuilder.UserHasPermissionTo(userId, permission);
+
+			// If there's anything other than one condition then fail.
+			if (mfSearchBuilder.Conditions.Count != 1)
+				Assert.Inconclusive("Only one search condition should exist");
+
+			// Retrieve the just-added condition.
+			var condition = mfSearchBuilder.Conditions[mfSearchBuilder.Conditions.Count];
+
+			// Ensure the condition type is correct.
+			Assert.AreEqual(MFConditionType.MFConditionTypeEqual, condition.ConditionType);
+
+			// Ensure the expression type is correct.
+			Assert.AreEqual(MFExpressionType.MFExpressionTypePermissions, condition.Expression.Type);
+
+			// Ensure the status value is correct.
+			Assert.AreEqual(permission, condition.Expression.DataPermissionsType);
+
+			// Ensure that the typed value is correct.
+			Assert.AreEqual(MFDataType.MFDatatypeLookup, condition.TypedValue.DataType);
+			Assert.AreEqual(userId, condition.TypedValue.GetLookupID());
+		}
+		
+		/// <summary>
+		/// Tests that calling
+		/// <see cref="MFSearchBuilderExtensionMethods.VisibleTo"/>
+		/// adds a valid search condition.
+		/// </summary>
+		[TestMethod]
+		public void VisibleToSearchConditionIsCorrect()
+		{
+			// Create the search builder.
+			var mfSearchBuilder = this.GetSearchBuilder();
+			
+			// Add the search condition for whether the user has the required permissions.
+			mfSearchBuilder.VisibleTo(25);
+
+			// If there's anything other than one condition then fail.
+			if (mfSearchBuilder.Conditions.Count != 1)
+				Assert.Inconclusive("Only one search condition should exist");
+
+			// Retrieve the just-added condition.
+			var condition = mfSearchBuilder.Conditions[mfSearchBuilder.Conditions.Count];
+
+			// Ensure the condition type is correct.
+			Assert.AreEqual(MFConditionType.MFConditionTypeEqual, condition.ConditionType);
+
+			// Ensure the expression type is correct.
+			Assert.AreEqual(MFExpressionType.MFExpressionTypePermissions, condition.Expression.Type);
+
+			// Ensure the status value is correct.
+			Assert.AreEqual(MFPermissionsExpressionType.MFVisibleTo, condition.Expression.DataPermissionsType);
+
+			// Ensure that the typed value is correct.
+			Assert.AreEqual(MFDataType.MFDatatypeLookup, condition.TypedValue.DataType);
+			Assert.AreEqual(25, condition.TypedValue.GetLookupID());
+		}
+		
+		/// <summary>
+		/// Tests that calling
+		/// <see cref="MFSearchBuilderExtensionMethods.EditableBy"/>
+		/// adds a valid search condition.
+		/// </summary>
+		[TestMethod]
+		public void EditableBySearchConditionIsCorrect()
+		{
+			// Create the search builder.
+			var mfSearchBuilder = this.GetSearchBuilder();
+			
+			// Add the search condition for whether the user has the required permissions.
+			mfSearchBuilder.EditableBy(25);
+
+			// If there's anything other than one condition then fail.
+			if (mfSearchBuilder.Conditions.Count != 1)
+				Assert.Inconclusive("Only one search condition should exist");
+
+			// Retrieve the just-added condition.
+			var condition = mfSearchBuilder.Conditions[mfSearchBuilder.Conditions.Count];
+
+			// Ensure the condition type is correct.
+			Assert.AreEqual(MFConditionType.MFConditionTypeEqual, condition.ConditionType);
+
+			// Ensure the expression type is correct.
+			Assert.AreEqual(MFExpressionType.MFExpressionTypePermissions, condition.Expression.Type);
+
+			// Ensure the status value is correct.
+			Assert.AreEqual(MFPermissionsExpressionType.MFEditableBy, condition.Expression.DataPermissionsType);
+
+			// Ensure that the typed value is correct.
+			Assert.AreEqual(MFDataType.MFDatatypeLookup, condition.TypedValue.DataType);
+			Assert.AreEqual(25, condition.TypedValue.GetLookupID());
+		}
+		
+		/// <summary>
+		/// Tests that calling
+		/// <see cref="MFSearchBuilderExtensionMethods.DeletableBy"/>
+		/// adds a valid search condition.
+		/// </summary>
+		[TestMethod]
+		public void DeletableBySearchConditionIsCorrect()
+		{
+			// Create the search builder.
+			var mfSearchBuilder = this.GetSearchBuilder();
+			
+			// Add the search condition for whether the user has the required permissions.
+			mfSearchBuilder.DeletableBy(25);
+
+			// If there's anything other than one condition then fail.
+			if (mfSearchBuilder.Conditions.Count != 1)
+				Assert.Inconclusive("Only one search condition should exist");
+
+			// Retrieve the just-added condition.
+			var condition = mfSearchBuilder.Conditions[mfSearchBuilder.Conditions.Count];
+
+			// Ensure the condition type is correct.
+			Assert.AreEqual(MFConditionType.MFConditionTypeEqual, condition.ConditionType);
+
+			// Ensure the expression type is correct.
+			Assert.AreEqual(MFExpressionType.MFExpressionTypePermissions, condition.Expression.Type);
+
+			// Ensure the status value is correct.
+			Assert.AreEqual(MFPermissionsExpressionType.MFDeletableBy, condition.Expression.DataPermissionsType);
+
+			// Ensure that the typed value is correct.
+			Assert.AreEqual(MFDataType.MFDatatypeLookup, condition.TypedValue.DataType);
+			Assert.AreEqual(25, condition.TypedValue.GetLookupID());
+		}
+
+	}
+}
---
+++ MFiles.VAF.Extensions/ExtensionMethods/MFSearchBuilderExtensionMethods/Permissions.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using MFiles.VAF.Common;
+using MFilesAPI;
+
+namespace MFiles.VAF.Extensions
+{
+	/// <summary>
+	/// Extension methods for the <see cref="MFSearchBuilder"/> class.
+	/// </summary>
+	// ReSharper disable once InconsistentNaming
+	public static partial class MFSearchBuilderExtensionMethods
+	{
+
+		#region User has permission to (generic)
+
+		/// <summary>
+		/// Adds a <see cref="SearchCondition"/> to the collection for whether the specified user has the specified permission on an object.
+		/// </summary>
+		/// <param name="searchBuilder">The <see cref="MFSearchBuilder"/> to add the condition to.</param>
+		/// <param name="userId">The ID of the user in question.</param>
+		/// <param name="permission">The permission that the user should have on the object.</param>
+		/// <param name="conditionType">What type of search to execute (defaults to <see cref="MFConditionType.MFConditionTypeEqual"/>).</param>
+		/// <returns>The <paramref name="searchBuilder"/> provided, for chaining.</returns>
+		public static MFSearchBuilder UserHasPermissionTo
+		(
+			this MFSearchBuilder searchBuilder,
+			int userId,
+			MFPermissionsExpressionType permission,
+			MFConditionType conditionType = MFConditionType.MFConditionTypeEqual
+		)
+		{
+			// Sanity.
+			if (null == searchBuilder)
+				throw new ArgumentNullException(nameof(searchBuilder));
+
+			// Create the search condition.
+			var searchCondition = new SearchCondition
+			{
+				ConditionType = conditionType
+			};
+
+			// Set up the permission expression.
+			searchCondition.Expression.SetPermissionExpression
+			(
+				permission
+			);
+
+			// Set the condition value to the lookup.
+			var lookup = new MFilesAPI.Lookup()
+			{
+				ObjectType = (int)MFBuiltInValueList.MFBuiltInValueListUsers,
+				Item = userId
+			};
+			searchCondition.TypedValue.SetValue(MFDataType.MFDatatypeLookup, lookup);
+
+			// Add the search condition to the collection.
+			searchBuilder.Conditions.Add(-1, searchCondition);
+
+			// Return the search builder for chaining.
+			return searchBuilder;
+		}
+
+		#endregion
+
+		#region Visible to
+
+		/// <summary>
+		/// Adds a <see cref="SearchCondition"/> to the collection for whether the specified user can see an object.
+		/// </summary>
+		/// <param name="searchBuilder">The <see cref="MFSearchBuilder"/> to add the condition to.</param>
+		/// <param name="userId">The ID of the user that should be able to see the object.</param>
+		/// <param name="conditionType">What type of search to execute (defaults to <see cref="MFConditionType.MFConditionTypeEqual"/>).</param>
+		/// <returns>The <paramref name="searchBuilder"/> provided, for chaining.</returns>
+		public static MFSearchBuilder VisibleTo
+		(
+			this MFSearchBuilder searchBuilder,
+			int userId,
+			MFConditionType conditionType = MFConditionType.MFConditionTypeEqual
+		)
+		{
+			// Use UserHasPermissionTo method.
+			return searchBuilder.UserHasPermissionTo
+			(
+				userId,
+				MFPermissionsExpressionType.MFVisibleTo,
+				conditionType
+			);
+		}
+
+		#endregion
+
+		#region Editable by
+
+		/// <summary>
+		/// Adds a <see cref="SearchCondition"/> to the collection for whether the specified user can edit an object.
+		/// </summary>
+		/// <param name="searchBuilder">The <see cref="MFSearchBuilder"/> to add the condition to.</param>
+		/// <param name="userId">The ID of the user that should be able to edit the object.</param>
+		/// <param name="conditionType">What type of search to execute (defaults to <see cref="MFConditionType.MFConditionTypeEqual"/>).</param>
+		/// <returns>The <paramref name="searchBuilder"/> provided, for chaining.</returns>
+		public static MFSearchBuilder EditableBy
+		(
+			this MFSearchBuilder searchBuilder,
+			int userId,
+			MFConditionType conditionType = MFConditionType.MFConditionTypeEqual
+		)
+		{
+			// Use UserHasPermissionTo method.
+			return searchBuilder.UserHasPermissionTo
+			(
+				userId,
+				MFPermissionsExpressionType.MFEditableBy,
+				conditionType
+			);
+		}
+
+		#endregion
+
+		#region Deletable by
+
+		/// <summary>
+		/// Adds a <see cref="SearchCondition"/> to the collection for whether the specified user can delete an object.
+		/// </summary>
+		/// <param name="searchBuilder">The <see cref="MFSearchBuilder"/> to add the condition to.</param>
+		/// <param name="userId">The ID of the user that should be able to delete the object.</param>
+		/// <param name="conditionType">What type of search to execute (defaults to <see cref="MFConditionType.MFConditionTypeEqual"/>).</param>
+		/// <returns>The <paramref name="searchBuilder"/> provided, for chaining.</returns>
+		public static MFSearchBuilder DeletableBy
+		(
+			this MFSearchBuilder searchBuilder,
+			int userId,
+			MFConditionType conditionType = MFConditionType.MFConditionTypeEqual
+		)
+		{
+			// Use UserHasPermissionTo method.
+			return searchBuilder.UserHasPermissionTo
+			(
+				userId,
+				MFPermissionsExpressionType.MFDeletableBy,
+				conditionType
+			);
+		}
+
+		#endregion
+	}
+}
