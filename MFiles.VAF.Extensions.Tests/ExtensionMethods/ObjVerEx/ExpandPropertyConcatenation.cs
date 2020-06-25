791652f803ad397eba5b3d52d88a8ca79c50ae24
Craig Hawker <craig.hawker@gmail.com>
2020-06-25 16:17:37 +0100

    Initial implementation of ExpandPropertyConcatenation Tests for the above.

---
+++ MFiles.VAF.Extensions.Tests/ExtensionMethods/ObjVerEx/ExpandPropertyConcatenation.cs
@@ -0,0 +1,193 @@
+﻿using MFiles.VAF.Extensions.ExtensionMethods;
+using MFilesAPI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFiles.VAF.Extensions.Tests.ExtensionMethods.ObjVerEx
+{
+	[TestClass]
+	public class ExpandPropertyConcatenation
+		: TestBaseWithVaultMock
+	{
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void NullObjVerExThrows()
+		{
+			((Common.ObjVerEx)null).ExpandPropertyConcatenation("hello world");
+		}
+
+		[TestMethod]
+		public void NullStringReturnsEmptyString()
+		{
+			Assert.AreEqual(string.Empty, new Common.ObjVerEx().ExpandPropertyConcatenation(null));
+		}
+
+		[TestMethod]
+		public void BlankStringReturnsEmptyString()
+		{
+			Assert.AreEqual(string.Empty, new Common.ObjVerEx().ExpandPropertyConcatenation(""));
+		}
+
+		[TestMethod]
+		public void SingleInternalID()
+		{
+			var vaultMock = this.GetVaultMock();
+			var objectVersionAndPropertiesMock = this.GetObjectVersionAndPropertiesMock(vaultMock);
+			var objVerEx = new Common.ObjVerEx(vaultMock.Object, objectVersionAndPropertiesMock.Object);
+			Assert.AreEqual("hello 123 world", objVerEx.ExpandPropertyConcatenation("hello %INTERNALID% world"));
+		}
+
+		[TestMethod]
+		public void MultipleInternalID()
+		{
+			var vaultMock = this.GetVaultMock();
+			var objectVersionAndPropertiesMock = this.GetObjectVersionAndPropertiesMock(vaultMock);
+			var objVerEx = new Common.ObjVerEx(vaultMock.Object, objectVersionAndPropertiesMock.Object);
+			Assert.AreEqual("hello 123 world 123", objVerEx.ExpandPropertyConcatenation("hello %INTERNALID% world %INTERNALID%"));
+		}
+
+		[TestMethod]
+		public void SingleExternalID()
+		{
+			var vaultMock = this.GetVaultMock();
+			var objectVersionAndPropertiesMock = this.GetObjectVersionAndPropertiesMock(vaultMock);
+			var objVerEx = new Common.ObjVerEx(vaultMock.Object, objectVersionAndPropertiesMock.Object);
+			Assert.AreEqual("hello 123ABCDEF123 world", objVerEx.ExpandPropertyConcatenation("hello %EXTERNALID% world"));
+		}
+
+		[TestMethod]
+		public void MultipleExternalID()
+		{
+			var vaultMock = this.GetVaultMock();
+			var objectVersionAndPropertiesMock = this.GetObjectVersionAndPropertiesMock(vaultMock);
+			var objVerEx = new Common.ObjVerEx(vaultMock.Object, objectVersionAndPropertiesMock.Object);
+			Assert.AreEqual("hello 123ABCDEF123 world 123ABCDEF123", objVerEx.ExpandPropertyConcatenation("hello %EXTERNALID% world %EXTERNALID%"));
+		}
+
+		[TestMethod]
+		public void PropertyIDWithNumber()
+		{
+			var vaultMock = this.GetVaultMock();
+			var objectVersionAndPropertiesMock = this.GetObjectVersionAndPropertiesMock
+			(
+				vaultMock,
+				propertyValues: new Tuple<int, MFDataType, object>(0, MFDataType.MFDatatypeText, "hello world")
+			);
+			var objVerEx = new Common.ObjVerEx(vaultMock.Object, objectVersionAndPropertiesMock.Object);
+			Assert.AreEqual("document is called: hello world", objVerEx.ExpandPropertyConcatenation("document is called: %PROPERTY_0%"));
+		}
+
+		[TestMethod]
+		public void PropertyIDWithAlias()
+		{
+			var vaultMock = this.GetVaultMock
+			(
+				new Tuple<string, int>("MF.PD.Title", 0)
+			);
+			var objectVersionAndPropertiesMock = this.GetObjectVersionAndPropertiesMock
+			(
+				vaultMock,
+				propertyValues: new Tuple<int, MFDataType, object>(0, MFDataType.MFDatatypeText, "hello world")
+			);
+			var objVerEx = new Common.ObjVerEx(vaultMock.Object, objectVersionAndPropertiesMock.Object);
+			Assert.AreEqual("document is called: hello world", objVerEx.ExpandPropertyConcatenation("document is called: %PROPERTY_{MF.PD.Title}%"));
+		}
+
+		//[TestMethod]
+		//public void IndirectProperties()
+		//{
+		//	var vaultMock = this.GetVaultMock
+		//	(
+		//		new Tuple<string, int>("MF.PD.Company", 123),
+		//		new Tuple<string, int>("Name", 0)
+		//	);
+		//	var related2 = this.GetObjectVersionAndPropertiesMock
+		//	(
+		//		vaultMock,
+		//		propertyValues: new Tuple<int, MFDataType, object>(0, MFDataType.MFDatatypeText, "hello world")
+		//	);
+		//	var related1 = this.GetObjectVersionAndPropertiesMock
+		//	(
+		//		vaultMock,
+		//		propertyValues: new Tuple<int, MFDataType, object>(0, MFDataType.MFDatatypeText, "hello world")
+		//	);
+		//	var source = this.GetObjectVersionAndPropertiesMock
+		//	(
+		//		vaultMock,
+		//		propertyValues: new []
+		//		{
+		//			new Tuple<int, MFDataType, object>(123, MFDataType.MFDatatypeLookup, "hello world")
+		//		}
+		//	);
+		//	var objVerEx = new Common.ObjVerEx(vaultMock.Object, objectVersionAndPropertiesMock.Object);
+		//	Assert.AreEqual("document is called: hello world", objVerEx.ExpandPropertyConcatenation("document is called: %PROPERTY_{MF.PD.Company}.PROPERTY_456.PROPERTY_{Name}%"));
+		//}
+
+		protected virtual Mock<Vault> GetVaultMock(params Tuple<string, int>[] propertyDefinitions)
+		{
+			var propertyDefOperationsMock = new Mock<VaultPropertyDefOperations>();
+			propertyDefOperationsMock
+				.Setup(o => o.GetPropertyDefIDByAlias(It.IsAny<string>()))
+				.Returns((string alias) =>
+				{
+					return propertyDefinitions
+						.FirstOrDefault(t => t.Item1 == alias)?
+						.Item2 ?? -1;
+				});
+
+			var vault = base.GetVaultMock();
+			vault.Setup(v => v.PropertyDefOperations).Returns(propertyDefOperationsMock.Object);
+			return vault;
+		}
+
+		protected virtual Mock<ObjectVersionAndProperties> GetObjectVersionAndPropertiesMock
+			(
+			Mock<Vault> vaultMock,
+			int objectTypeId = 0,
+			int objectId = 123,
+			int version = 1,
+			string externalId = "123ABCDEF123",
+			params Tuple<int, MFDataType, object>[] propertyValues
+			)
+		{
+			var objectVersionAndPropertiesMock = new Mock<ObjectVersionAndProperties>();
+			objectVersionAndPropertiesMock
+				.Setup(o => o.ObjVer)
+				.Returns(() =>
+				{
+					var objVer = new ObjVer();
+					objVer.SetIDs(objectTypeId, objectId, version);
+					return objVer;
+				});
+			objectVersionAndPropertiesMock
+				.Setup(o => o.VersionData)
+				.Returns(() =>
+				{
+					var data = new Mock<ObjectVersion>();
+					data.Setup(o => o.DisplayIDAvailable).Returns(true);
+					data.Setup(o => o.DisplayID).Returns(externalId);
+					return data.Object;
+				});
+			objectVersionAndPropertiesMock
+				.Setup(o => o.Properties)
+				.Returns(() =>
+				{
+					var data = new PropertyValues();
+					foreach(var tuple in propertyValues ?? new Tuple<int, MFDataType, object>[0])
+					{
+						var pv = new PropertyValue();
+						pv.PropertyDef = tuple.Item1;
+						pv.Value.SetValue(tuple.Item2, tuple.Item3);
+						data.Add(-1, pv);
+					}
+					return data;
+				});
+			return objectVersionAndPropertiesMock;
+		}
+	}
+}
---
+++ MFiles.VAF.Extensions/ExtensionMethods/ObjVerEx/ExpandPropertyConcatenation.cs
@@ -0,0 +1,94 @@
+﻿using MFiles.VAF.Common;
+using System;
+using System.Text.RegularExpressions;
+
+namespace MFiles.VAF.Extensions.ExtensionMethods
+{
+	public static partial class ObjVerExExtensionMethods
+	{
+		/// <summary>
+		/// The regular expression used to extract property matches ("%PROPERTY_0%").
+		/// </summary>
+		private static Regex extractPlaceholders = new Regex(
+			"(?<replacementText>(?<prefix>\\%)(?<reference>(?<type>PROPERTY_)(\\{?(?<aliasorid>[^%]+?)\\}?)?\\.?)+(?<suffix>\\%))",
+			RegexOptions.Multiline
+			| RegexOptions.ExplicitCapture
+			| RegexOptions.CultureInvariant
+			| RegexOptions.Compiled
+		);
+
+		/// <summary>
+		/// Expands a simple string using similar logic to the "concatenated properties"
+		/// functionality in M-Files Admin.
+		/// More lightweight than <see cref="ObjVerEx.ExpandPlaceholderText(string, bool, bool)"/> by only supporting
+		/// simple property expressions (%PROPERTY_123%, %PROPERTY_{Alias}%, %PROPERTY_{Alias1}.PROPERTY_{Alias2}%, %EXTERNALID% and %INTERNALID%).
+		/// </summary>
+		/// <param name="objVerEx">The object containing information to use for the expansion.</param>
+		/// <param name="concatenationString">The string containing the placeholders.</param>
+		/// <returns>The expanded string.</returns>
+		public static string ExpandPropertyConcatenation
+		(
+			this ObjVerEx objVerEx,
+			string concatenationString
+		)
+		{	
+			// Sanity.
+			if (null == objVerEx)
+				throw new ArgumentNullException(nameof(objVerEx));
+			if (string.IsNullOrWhiteSpace(concatenationString))
+				return string.Empty;
+
+			// Replace simple strings.
+			concatenationString = concatenationString.Replace("%INTERNALID%", objVerEx.ID.ToString());
+			concatenationString = concatenationString.Replace("%EXTERNALID%", objVerEx.Info.DisplayID);
+
+			// Try and get all property placeholders.
+			return extractPlaceholders.Replace(concatenationString, (Match match) =>
+			{
+				// Sanity.
+				if(!match.Success)
+					return match.Value;
+
+				// Extract the vault structural references.
+				var aliasOrIds = match.Groups["aliasorid"];
+
+				// No type, die.
+				if(!aliasOrIds.Success)
+					return match.Value;
+
+				// Iterate over the references to build up the string.
+				var host = objVerEx;
+				for(var i =0; i<aliasOrIds.Captures.Count; i++)
+				{
+					// Get the vault structural reference string.
+					var propertyReference = aliasOrIds.Captures[i].Value;
+
+					// If it's an integer then it's a property id.
+					if (Int32.TryParse(propertyReference, out int propertyId))
+						return objVerEx.GetPropertyText(propertyId);
+
+					// It's an alias - resolve it to an id.
+					propertyId = objVerEx
+						.Vault
+						.PropertyDefOperations
+						.GetPropertyDefIDByAlias(propertyReference);
+
+					// Not found == empty string.
+					if (-1 == propertyId)
+						return string.Empty;
+
+					// If we are on the last one then get the property value.
+					if (i == aliasOrIds.Captures.Count - 1)
+						return host.GetPropertyText(propertyId);
+
+					// Otherwise, alter the host.
+					host = host.GetDirectReference(propertyId);
+				}
+
+				// No replacement.
+				return match.Value;
+			});
+
+		}
+	}
+}
