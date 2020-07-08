640cf1f325845985e59322bd06696078512f4c5f
Craig Hawker <craig.hawker@gmail.com>
2020-07-08 14:06:04 +0100

    Comments.

--- MFiles.VAF.Extensions/MultiServerMode/ConfigurableVaultApplicationBase.cs
+++ MFiles.VAF.Extensions/MultiServerMode/ConfigurableVaultApplicationBase.cs
@@ -7,18 +7,32 @@ using System;
 
 namespace MFiles.VAF.Extensions.MultiServerMode
 {
+	/// <summary>
+	/// A base class that automatically implements the pattern required for broadcasting
+	/// configuration changes to other servers.
+	/// </summary>
+	/// <typeparam name="TSecureConfiguration">The configuration type.</typeparam>
+	/// <remarks>See https://developer.m-files.com/Frameworks/Vault-Application-Framework/Multi-Server-Mode/#configuration-changes for further details.</remarks>
 	public abstract class ConfigurableVaultApplicationBase<TSecureConfiguration>
 		: MFiles.VAF.Core.ConfigurableVaultApplicationBase<TSecureConfiguration>
 	where TSecureConfiguration : class, new()
 	{
-		private string rebroadcastQueueId = null;
+		/// <summary>
+		/// The rebroadcast queue.  Populated during the first call to
+		/// <see cref="GetRebroadcastQueueId"/>.
+		/// </summary>
+		private string configurationRebroadcastQueueId = null;
 
 		/// <inheritdoc />
 		public override string GetRebroadcastQueueId()
 		{
-			if (string.IsNullOrWhiteSpace(this.rebroadcastQueueId))
-				this.rebroadcastQueueId = this.EnableConfigurationRebroadcasting();
-			return this.rebroadcastQueueId;
+			// If we do not have a rebroadcast queue for the configuration data
+			// then create one.
+			if (string.IsNullOrWhiteSpace(this.configurationRebroadcastQueueId))
+				this.configurationRebroadcastQueueId = this.EnableConfigurationRebroadcasting();
+
+			// Return the broadcast queue.
+			return this.configurationRebroadcastQueueId;
 		}
 
 	}
