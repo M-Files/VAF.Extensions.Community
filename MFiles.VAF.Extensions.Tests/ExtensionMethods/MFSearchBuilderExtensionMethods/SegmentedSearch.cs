464ead94a26b6c34c6474bc9ad06e9d4f09c268b
Aaron Hardin <aaron.hardin@m-files.com>
2020-10-29 12:26:16 -0500

    Increase test accuracy
    Remove mocking at class level and add at method level for better specific tests

--- MFiles.VAF.Extensions.Tests/ExtensionMethods/MFSearchBuilderExtensionMethods/SegmentedSearch.cs
+++ MFiles.VAF.Extensions.Tests/ExtensionMethods/MFSearchBuilderExtensionMethods/SegmentedSearch.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using MFiles.VAF.Common;
 using MFilesAPI;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -17,7 +16,7 @@ namespace MFiles.VAF.Extensions.Tests.ExtensionMethods.MFSearchBuilderExtensionM
 		/// executes a segmented search.
 		/// </summary>
 		[TestMethod]
-		public void SegmentedSearchForEach()
+		public void SegmentedSearchForEachEx_Empty()
 		{
 			// Create the search builder.
 			var mfSearchBuilder = this.GetSearchBuilder();
@@ -25,15 +24,13 @@ namespace MFiles.VAF.Extensions.Tests.ExtensionMethods.MFSearchBuilderExtensionM
 			// Ensure it has no items in the collection.
 			Assert.AreEqual(0, mfSearchBuilder.Conditions.Count);
 
-			// Add the search condition for the extension.
-			mfSearchBuilder.ObjType(0);
-
+			// Count up the items in the vault (should be none!).
 			var count1 = 0;
 			var count2 = mfSearchBuilder.ForEachEx( obj => { count1 += 1; } );
 
-			// Ensure that there is one item in the vault.
-			Assert.AreEqual(1, count1);
-			Assert.AreEqual(1, count2);
+			// Ensure that nothing is returned as there are no items.
+			Assert.AreEqual(0, count1);
+			Assert.AreEqual(0, count2);
 		}
 
 		[TestMethod]
@@ -45,13 +42,10 @@ namespace MFiles.VAF.Extensions.Tests.ExtensionMethods.MFSearchBuilderExtensionM
 			// Ensure it has no items in the collection.
 			Assert.AreEqual(0, mfSearchBuilder.Conditions.Count);
 
-			// Add the search condition for the extension.
-			mfSearchBuilder.ObjType(0);
-
 			var count = mfSearchBuilder.SegmentedCount();
 
-			// Ensure that there is one item in the vault.
-			Assert.AreEqual(1, count);
+			// Ensure that nothing is returned as there are no items.
+			Assert.AreEqual(0, count);
 		}
 
 		[TestMethod]
@@ -237,6 +231,17 @@ namespace MFiles.VAF.Extensions.Tests.ExtensionMethods.MFSearchBuilderExtensionM
 
 		[TestMethod]
 		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void MinObjIdZeroSegmentThrows()
+		{
+			// Create the search builder.
+			var mfSearchBuilder = this.GetSearchBuilder();
+
+			// Zero segment index should throw because this method is expected to be used for finding out if any future segment has results
+			mfSearchBuilder.MinObjId(0, 1);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
 		public void MinObjIdNegativeSegmentSizeThrows()
 		{
 			// Create the search builder.
@@ -412,6 +417,200 @@ namespace MFiles.VAF.Extensions.Tests.ExtensionMethods.MFSearchBuilderExtensionM
 			);
 		}
 
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void ForEachSegmentNegativeTimeoutThrows()
+		{
+			// Create the search builder.
+			var mfSearchBuilder = this.GetSearchBuilder();
+
+			// Attempt to execute with negative search timeout.
+			mfSearchBuilder.ForEachSegment
+			(
+				( v, sc ) => { return 0; },
+				searchTimeoutInSeconds: -1
+			);
+		}
+
+		[TestMethod]
+		public void ForEachSegmentCallsFirstSegment()
+		{
+			// Get the standard vault mock.
+			var vaultMock = this.GetVaultMock();
+
+			// Set up the search operations mock to return one item.
+			var searchOperationsMock = new Mock<VaultObjectSearchOperations>();
+			searchOperationsMock.SetupAllProperties();
+			searchOperationsMock
+				.Setup(m => m.SearchForObjectsByConditionsEx(It.IsAny<SearchConditions>(), It.IsAny<MFSearchFlags>(), It.IsAny<bool>(), It.IsAny<int>(), It.IsAny<int>()))
+				.Returns((SearchConditions searchConditions, MFSearchFlags searchFlags, bool sortResults, int maxResults, int timeout) =>
+				{
+					// This will be called once to check that there are no additional items.
+
+					// Confirm arguments are correct.
+					Assert.IsNotNull(searchConditions);
+					Assert.AreEqual(MFSearchFlags.MFSearchFlagDisableRelevancyRanking, searchFlags);
+					Assert.AreEqual(false, sortResults);
+					Assert.AreEqual(1, maxResults); // Are there ANY in a subsequent segment?
+					Assert.AreEqual(Extensions.MFSearchBuilderExtensionMethods.DefaultSearchTimeoutInSeconds, timeout);
+					Assert.AreEqual(1, searchConditions.Count);
+
+					// Check that search condition.
+					var searchCondition = searchConditions[1];
+					Assert.IsNotNull(searchCondition);
+					Assert.AreEqual(MFConditionType.MFConditionTypeGreaterThanOrEqual, searchCondition.ConditionType);
+					Assert.AreEqual(MFStatusType.MFStatusTypeObjectID, searchCondition.Expression.DataStatusValueType);
+
+					// This is the minimum object id search condition, so for the first segment we expect a value of 1 * segmentSize
+					Assert.AreEqual
+					(
+						1 * Extensions.MFSearchBuilderExtensionMethods.DefaultNumberOfItemsInSegment, // Anything outside of the first segment.
+						searchCondition.TypedValue.Value
+					);
+
+					// No more items.
+					return new ObjectSearchResults();
+				});
+			vaultMock.Setup(m => m.ObjectSearchOperations).Returns(searchOperationsMock.Object);
+
+			// Create the search builder.
+			var searchBuilder = new MFSearchBuilder(vaultMock.Object);
+
+			// Execute ForEachSegment.
+			var itemsCount = searchBuilder.ForEachSegment
+			(
+				(v, s) =>
+				{
+					// Ensure that the search condition has been added.
+					Assert.IsNotNull(s);
+					Assert.AreEqual(1, s.Count);
+
+					// Check that search condition.
+					var searchCondition = s[1];
+					Assert.IsNotNull(searchCondition);
+					Assert.AreEqual(MFConditionType.MFConditionTypeEqual, searchCondition.ConditionType);
+					Assert.AreEqual(MFExpressionType.MFExpressionTypeObjectIDSegment, searchCondition.Expression.Type);
+					Assert.AreEqual(Extensions.MFSearchBuilderExtensionMethods.DefaultNumberOfItemsInSegment, searchCondition.Expression.DataObjectIDSegmentSegmentSize);
+
+					// This is the segment condition, so we expect the index of the segment to be 0 (the first segment)
+					Assert.AreEqual(0, searchCondition.TypedValue.Value);
+
+					// Return no items hit (will cause system to check whether there are any more items).
+					return 0;
+				}
+			);
+
+			// We expect SearchForObjectsByConditionsEx to be hit once.
+			searchOperationsMock.Verify
+				(
+					m => m.SearchForObjectsByConditionsEx(It.IsAny<SearchConditions>(), It.IsAny<MFSearchFlags>(), It.IsAny<bool>(), It.IsAny<int>(), It.IsAny<int>()),
+					Times.Exactly(1)
+				);
+
+			// Ensure that all expectations are met.
+			vaultMock.Verify();
+
+			// Ensure that we no items (nothing in the vault!)
+			Assert.AreEqual(0, itemsCount);
+		}
+
+		[TestMethod]
+		public void ForEachSegmentCallsSecondSegment()
+		{
+			// Get the standard vault mock.
+			var vaultMock = this.GetVaultMock();
+
+			// For keeping track of the current segment
+			var segment = 0;
+
+			// Set up the search operations mock to return one item.
+			var searchOperationsMock = new Mock<VaultObjectSearchOperations>();
+			searchOperationsMock.SetupAllProperties();
+			searchOperationsMock
+				.Setup(m => m.SearchForObjectsByConditionsEx(It.IsAny<SearchConditions>(), It.IsAny<MFSearchFlags>(), It.IsAny<bool>(), It.IsAny<int>(), It.IsAny<int>()))
+				.Returns((SearchConditions searchConditions, MFSearchFlags searchFlags, bool sortResults, int maxResults, int timeout) =>
+				{
+					// This will be called once to check that there are no additional items.
+
+					// Confirm arguments are correct.
+					Assert.IsNotNull(searchConditions);
+					Assert.AreEqual(MFSearchFlags.MFSearchFlagDisableRelevancyRanking, searchFlags);
+					Assert.AreEqual(false, sortResults);
+					Assert.AreEqual(1, maxResults); // Are there ANY in a subsequent segment?
+					Assert.AreEqual(Extensions.MFSearchBuilderExtensionMethods.DefaultSearchTimeoutInSeconds, timeout);
+					Assert.AreEqual(1, searchConditions.Count);
+
+					// Check that search condition.
+					var searchCondition = searchConditions[1];
+					Assert.IsNotNull(searchCondition);
+					Assert.AreEqual(MFConditionType.MFConditionTypeGreaterThanOrEqual, searchCondition.ConditionType);
+					Assert.AreEqual(MFStatusType.MFStatusTypeObjectID, searchCondition.Expression.DataStatusValueType);
+
+					// This is the minimum object id search condition
+					// We should only hit this for the second segment, so for the second segment we expect a value of 2 * segmentSize
+					Assert.AreEqual
+					(
+						2 * Extensions.MFSearchBuilderExtensionMethods.DefaultNumberOfItemsInSegment, // Anything after the second segment.
+						searchCondition.TypedValue.Value
+					);
+
+					// No more items.
+					return new ObjectSearchResults();
+				});
+			vaultMock.Setup(m => m.ObjectSearchOperations).Returns(searchOperationsMock.Object);
+
+			// Create the search builder.
+			var searchBuilder = new MFSearchBuilder(vaultMock.Object);
+
+			// Execute ForEachSegment.
+			var itemsCount = searchBuilder.ForEachSegment
+			(
+				(v, s) =>
+				{
+					// Get the current segment and increment out counter
+					var currentSegment = segment;
+					++segment;
+
+					// Ensure that the search condition has been added.
+					Assert.IsNotNull(s);
+					Assert.AreEqual(1, s.Count);
+
+					// Check that search condition.
+					var searchCondition = s[1];
+					Assert.IsNotNull(searchCondition);
+					Assert.AreEqual(MFConditionType.MFConditionTypeEqual, searchCondition.ConditionType);
+					Assert.AreEqual(MFExpressionType.MFExpressionTypeObjectIDSegment, searchCondition.Expression.Type);
+					Assert.AreEqual(Extensions.MFSearchBuilderExtensionMethods.DefaultNumberOfItemsInSegment, searchCondition.Expression.DataObjectIDSegmentSegmentSize);
+
+					// This is the segment condition, so we expect the index of the segment to be the same as our counter
+					Assert.AreEqual(currentSegment, searchCondition.TypedValue.Value);
+
+					// Fake that we are hitting one item in the first segment
+					if (currentSegment == 0)
+						return 1;
+
+					// Return no items hit (will cause system to check whether there are any more items).
+					return 0;
+				}
+			);
+
+			// We expect SearchForObjectsByConditionsEx to be hit once.
+			searchOperationsMock.Verify
+				(
+					m => m.SearchForObjectsByConditionsEx(It.IsAny<SearchConditions>(), It.IsAny<MFSearchFlags>(), It.IsAny<bool>(), It.IsAny<int>(), It.IsAny<int>()),
+					Times.Exactly(1)
+				);
+
+			// Ensure that all expectations are met.
+			vaultMock.Verify();
+
+			// We processed segment 0 and found no items in or after segment 1 so our counter should end on 2 (gets incremented when looking at segment 1)
+			Assert.AreEqual(2, segment);
+
+			// Ensure that we hit one item (not really, but it causes us to go into the second segment)
+			Assert.AreEqual(1, itemsCount);
+		}
+
 		#endregion
 
 		protected override Mock<Vault> GetVaultMock()
@@ -420,91 +619,13 @@ namespace MFiles.VAF.Extensions.Tests.ExtensionMethods.MFSearchBuilderExtensionM
 			var vaultMock = base.GetVaultMock();
 
 			// Set up the search operations mock.
-			var objectSearchOperationsMock = new Mock<VaultObjectSearchOperations>();
+			var searchOperationsMock = new Mock<VaultObjectSearchOperations>();
+			searchOperationsMock.SetupAllProperties();
+			searchOperationsMock
+				.Setup(m => m.SearchForObjectsByConditionsEx(It.IsAny<SearchConditions>(), It.IsAny<MFSearchFlags>(), It.IsAny<bool>(), It.IsAny<int>(), It.IsAny<int>()))
+				.Returns(new ObjectSearchResults());
+			vaultMock.Setup(m => m.ObjectSearchOperations).Returns(searchOperationsMock.Object);
 
-			// Set up the SearchForObjectsByConditionsEx method.
-			objectSearchOperationsMock
-				.Setup( m => m.SearchForObjectsByConditionsEx( It.IsAny<SearchConditions>(), It.IsAny<MFSearchFlags>(), It.IsAny<bool>(), It.IsAny<int>(), It.IsAny<int>() ) )
-				.Returns( ( SearchConditions searchConditions, MFSearchFlags searchFlags, bool sortResults, int maxResults, int timeout ) =>
-				{
-					if (searchConditions.Count > 0 )
-					{
-						var searchConditionsList = searchConditions.Cast<SearchCondition>().ToList();
-						var minIdCondition = searchConditionsList.FirstOrDefault( sc => sc.Expression.Type == MFExpressionType.MFExpressionTypeStatusValue && sc.Expression.DataStatusValueType == MFStatusType.MFStatusTypeObjectID );
-						if( minIdCondition != null && (int) minIdCondition.TypedValue.Value > 0 )
-						{
-							return new ObjectSearchResults();
-						}
-
-						var segmentCondition = searchConditionsList.FirstOrDefault( sc => sc.Expression.Type == MFExpressionType.MFExpressionTypeObjectIDSegment );
-						if( segmentCondition != null && (int) segmentCondition.TypedValue.Value > 0 )
-						{
-							return new ObjectSearchResults();
-						}
-					}
-
-					var result = new Mock<ObjectVersion>();
-					result.Setup( m => m.ObjVer ).Returns( new ObjVer() );
-					var items = new List<ObjectVersion> { result.Object };
-					var results = new Mock<ObjectSearchResults>();
-					results.Setup( m => m.Count ).Returns( () => items.Count );
-					results.Setup( m => m[ It.IsAny<int>() ] ).Returns<int>( i => items.ElementAt( i - 1 ) ); // 1-indexed list
-					results.Setup( m => m.GetEnumerator() ).Returns( () => items.GetEnumerator() );
-
-					return results.Object;
-				} );
-
-			// Set up the GetObjectCountInSearch method.
-			objectSearchOperationsMock
-				.Setup( m => m.GetObjectCountInSearch( It.IsAny<SearchConditions>(), It.IsAny<MFSearchFlags>() ) )
-				.Returns( ( SearchConditions searchConditions, MFSearchFlags searchFlags ) =>
-				{
-					if( searchConditions.Count > 0 )
-					{
-						var searchConditionsList = searchConditions.Cast<SearchCondition>().ToList();
-						var segmentCondition = searchConditionsList.FirstOrDefault( sc => sc.Expression.Type == MFExpressionType.MFExpressionTypeObjectIDSegment );
-						if( segmentCondition != null && (int) segmentCondition.TypedValue.Value > 0 )
-						{
-							return 0;
-						}
-					}
-
-					return 1;
-				} );
-
-			// Make the vault return the search operations mock as needed.
-			vaultMock
-				.SetupGet( m => m.ObjectSearchOperations )
-				.Returns( objectSearchOperationsMock.Object );
-
-			// Set up the object property operations mock.
-			var objectPropertyOperationsMock = new Mock<VaultObjectPropertyOperations>();
-
-			objectPropertyOperationsMock
-				.Setup( m => m.GetPropertiesOfMultipleObjects( It.IsAny<ObjVers>() ) )
-				.Returns( ( ObjVers objVers ) =>
-				{
-					var items = new List<PropertyValues>();
-					for( int i = 0; i < objVers.Count; i++ )
-					{
-						var result = new Mock<PropertyValues>();
-						items.Add( result.Object );
-					}
-
-					var results = new Mock<PropertyValuesOfMultipleObjects>();
-					results.Setup( m => m.Count ).Returns( () => items.Count );
-					results.Setup( m => m[ It.IsAny<int>() ] ).Returns<int>( i => items.ElementAt( i - 1 ) ); // 1-indexed list
-					results.Setup( m => m.GetEnumerator() ).Returns( () => items.GetEnumerator() );
-
-					return results.Object;
-				} );
-
-			// Make the vault return the object property operations mock as needed.
-			vaultMock
-				.SetupGet( m => m.ObjectPropertyOperations )
-				.Returns( objectPropertyOperationsMock.Object );
-
-			// Return the vault mock.
 			return vaultMock;
 		}
 	}
