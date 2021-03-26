131c8ddfc98b7cff8bdeed1fa831eb592f56d0fa
Craig Hawker <craig.hawker@m-files.com>
2021-03-26 16:18:32 +0000

    Split GetDashboardContent to partial file. Added TaskQueueBackgroundOperationManager as member on ConfigurableVaultApplicationBase (no need to implement manually!)

---
+++ MFiles.VAF.Extensions/MultiServerMode/ConfigurableVaultApplicationBase.GetDashboardContent.cs
@@ -0,0 +1,117 @@
+﻿// ReSharper disable once CheckNamespace
+using MFiles.VAF.Common.ApplicationTaskQueue;
+using MFiles.VAF.Configuration.AdminConfigurations;
+using MFiles.VAF.Configuration.Domain.Dashboards;
+using MFiles.VAF.Core;
+using MFiles.VAF.Extensions.MultiServerMode.ExtensionMethods;
+using MFiles.VAF.MultiserverMode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MFiles.VAF.Extensions.MultiServerMode
+{
+	public abstract partial class ConfigurableVaultApplicationBase<TSecureConfiguration>
+	{
+
+		/// <inheritdoc />
+		public override string GetDashboardContent(IConfigurationRequestContext context)
+		{
+			var dashboard = new StatusDashboard();
+
+			// If there's some base content then add that.
+			var baseContent = base.GetDashboardContent(context);
+			if (false == string.IsNullOrWhiteSpace(baseContent))
+				dashboard.AddContent(new DashboardCustomContent(baseContent));
+
+			// Do we have any background operation content?
+			var backgroundOperationContent = this.GetBackgroundOperationDashboardContent();
+			if(null != backgroundOperationContent)
+				dashboard.AddContent(backgroundOperationContent);
+
+			// Return the dashboard.
+			return dashboard.ToString();
+		}
+
+		/// <summary>
+		/// Returns the dashboard content showing background operation status.
+		/// </summary>
+		/// <returns>The dashboard content.  Can be null if no background operation managers or background operations.</returns>
+		public virtual DashboardPanel GetBackgroundOperationDashboardContent()
+		{
+			// Add each manager's data in turn.
+			var list = new DashboardList();
+			foreach (var manager in this.GetTaskQueueBackgroundOperationManagers() ?? new TaskQueueBackgroundOperationManager[0])
+			{
+				var listItems = manager.GetDashboardContent();
+				if (null == listItems)
+					continue;
+				list.Items.AddRange(listItems);
+			}
+
+			// Did we get anything?
+			if (0 == list.Items.Count)
+				list.Items.Add(new DashboardListItem()
+				{
+					Title = "There are no current background operations.",
+					StatusSummary = new Configuration.Domain.DomainStatusSummary()
+					{
+						Status = VAF.Configuration.Domain.DomainStatus.Undefined
+					}
+				});
+
+			// Return the panel.
+			return new DashboardPanel()
+			{
+				Title = "Background Operations",
+				InnerContent = list
+			};
+		}
+
+		/// <summary>
+		/// Returns <see cref="TaskQueueBackgroundOperationManager"/> instances declared on properties and fields
+		/// on this instance.
+		/// </summary>
+		/// <returns>A collection of background operation managers.</returns>
+		protected virtual IEnumerable<TaskQueueBackgroundOperationManager> GetTaskQueueBackgroundOperationManagers()
+		{
+			var taskQueueBackgroundOperationManagerType = typeof(TaskQueueBackgroundOperationManager);
+			TaskQueueBackgroundOperationManager value = null;
+
+			// Get all properties.
+			foreach (var p in this.GetType().GetProperties(System.Reflection.BindingFlags.Instance
+				 | System.Reflection.BindingFlags.FlattenHierarchy
+				 | System.Reflection.BindingFlags.Public
+				 | System.Reflection.BindingFlags.NonPublic)
+				.Where(p => p.CanRead && taskQueueBackgroundOperationManagerType.IsAssignableFrom(p.PropertyType)))
+			{
+				value = null;
+				try
+				{
+					value = p.GetValue(this) as TaskQueueBackgroundOperationManager;
+				}
+				catch { }
+				if (null != value)
+					yield return value;
+			}
+
+			// Get all fields.
+			foreach (var f in this.GetType().GetFields(System.Reflection.BindingFlags.Instance
+				 | System.Reflection.BindingFlags.FlattenHierarchy
+				 | System.Reflection.BindingFlags.Public
+				 | System.Reflection.BindingFlags.NonPublic)
+				.Where(f => !f.Name.EndsWith("_BackingField")  // Ignore backing fields for properties otherwise we report twice.
+				&& taskQueueBackgroundOperationManagerType.IsAssignableFrom(f.FieldType)))
+			{
+				value = null;
+				try
+				{
+					value = f.GetValue(this) as TaskQueueBackgroundOperationManager;
+				}
+				catch { }
+				if (null != value)
+					yield return value;
+			}
+		}
+	}
+}
--- MFiles.VAF.Extensions/MultiServerMode/ConfigurableVaultApplicationBase.cs
+++ MFiles.VAF.Extensions/MultiServerMode/ConfigurableVaultApplicationBase.cs
@@ -5,6 +5,7 @@ using MFiles.VAF.Configuration.Domain.Dashboards;
 using MFiles.VAF.Core;
 using MFiles.VAF.Extensions.MultiServerMode.ExtensionMethods;
 using MFiles.VAF.MultiserverMode;
+using MFilesAPI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,7 +18,7 @@ namespace MFiles.VAF.Extensions.MultiServerMode
 	/// </summary>
 	/// <typeparam name="TSecureConfiguration">The configuration type.</typeparam>
 	/// <remarks>See https://developer.m-files.com/Frameworks/Vault-Application-Framework/Multi-Server-Mode/#configuration-changes for further details.</remarks>
-	public abstract class ConfigurableVaultApplicationBase<TSecureConfiguration>
+	public abstract partial class ConfigurableVaultApplicationBase<TSecureConfiguration>
 		: MFiles.VAF.Core.ConfigurableVaultApplicationBase<TSecureConfiguration>, IUsesTaskQueue
 	where TSecureConfiguration : class, new()
 	{
@@ -32,6 +33,11 @@ namespace MFiles.VAF.Extensions.MultiServerMode
 		/// Populated during the first call to <see cref="GetRebroadcastQueueId"/>.
 		/// </summary>
 		protected AppTaskBatchProcessor ConfigurationRebroadcastTaskProcessor { get; private set; }
+
+		/// <summary>
+		/// The task queue background operation manager for this application.
+		/// </summary>
+		protected TaskQueueBackgroundOperationManager TaskQueueBackgroundOperationManager { get; private set; }
 
 		/// <inheritdoc />
 		public override string GetRebroadcastQueueId()
@@ -66,103 +72,34 @@ namespace MFiles.VAF.Extensions.MultiServerMode
 		#endregion
 
 		/// <inheritdoc />
-		public override string GetDashboardContent(IConfigurationRequestContext context)
+		protected override void StartApplication()
 		{
-			var dashboard = new StatusDashboard();
+			base.StartApplication();
 
-			// If there's some base content then add that.
-			var baseContent = base.GetDashboardContent(context);
-			if (false == string.IsNullOrWhiteSpace(baseContent))
-				dashboard.AddContent(new DashboardCustomContent(baseContent));
-
-			// Do we have any background operation content?
-			var backgroundOperationContent = this.GetBackgroundOperationDashboardContent();
-			if(null != backgroundOperationContent)
-				dashboard.AddContent(backgroundOperationContent);
-
-			// Return the dashboard.
-			return dashboard.ToString();
+			// Instantiate the background operation manager.
+			this.TaskQueueBackgroundOperationManager = new TaskQueueBackgroundOperationManager
+			(
+				this
+			);
 		}
 
-		/// <summary>
-		/// Returns the dashboard content showing background operation status.
-		/// </summary>
-		/// <returns>The dashboard content.  Can be null if no background operation managers or background operations.</returns>
-		public virtual DashboardPanel GetBackgroundOperationDashboardContent()
+		/// <inheritdoc />
+		protected override void UninitializeApplication(Vault vault)
 		{
-			// Add each manager's data in turn.
-			var list = new DashboardList();
-			foreach (var manager in this.GetTaskQueueBackgroundOperationManagers() ?? new TaskQueueBackgroundOperationManager[0])
+			// When the application is uninstalled we should cancel any future executions, to stop any oddities
+			// where a future application with the same name (or a re-install) picks up the old task queue.
+			// This is especially important where the task IDs may change but the queue stays the same,
+			// as this would throw exceptions because the new background operation manager cannot find
+			// a processor for the old task ID.
+			try
 			{
-				var listItems = manager.GetDashboardContent();
-				if (null == listItems)
-					continue;
-				list.Items.AddRange(listItems);
+				this.TaskQueueBackgroundOperationManager?.CancelFutureExecutions(remarks: "Application being uninstalled.");
+			}
+			catch
+			{
 			}
 
-			// Did we get anything?
-			if (0 == list.Items.Count)
-				list.Items.Add(new DashboardListItem()
-				{
-					Title = "There are no current background operations.",
-					StatusSummary = new Configuration.Domain.DomainStatusSummary()
-					{
-						Status = VAF.Configuration.Domain.DomainStatus.Undefined
-					}
-				});
-
-			// Return the panel.
-			return new DashboardPanel()
-			{
-				Title = "Background Operations",
-				InnerContent = list
-			};
-		}
-
-		/// <summary>
-		/// Returns <see cref="TaskQueueBackgroundOperationManager"/> instances declared on properties and fields
-		/// on this instance.
-		/// </summary>
-		/// <returns>A collection of background operation managers.</returns>
-		protected virtual IEnumerable<TaskQueueBackgroundOperationManager> GetTaskQueueBackgroundOperationManagers()
-		{
-			var taskQueueBackgroundOperationManagerType = typeof(TaskQueueBackgroundOperationManager);
-			TaskQueueBackgroundOperationManager value = null;
-
-			// Get all properties.
-			foreach (var p in this.GetType().GetProperties(System.Reflection.BindingFlags.Instance
-				 | System.Reflection.BindingFlags.FlattenHierarchy
-				 | System.Reflection.BindingFlags.Public
-				 | System.Reflection.BindingFlags.NonPublic)
-				.Where(p => p.CanRead && taskQueueBackgroundOperationManagerType.IsAssignableFrom(p.PropertyType)))
-			{
-				value = null;
-				try
-				{
-					value = p.GetValue(this) as TaskQueueBackgroundOperationManager;
-				}
-				catch { }
-				if (null != value)
-					yield return value;
-			}
-
-			// Get all fields.
-			foreach (var f in this.GetType().GetFields(System.Reflection.BindingFlags.Instance
-				 | System.Reflection.BindingFlags.FlattenHierarchy
-				 | System.Reflection.BindingFlags.Public
-				 | System.Reflection.BindingFlags.NonPublic)
-				.Where(f => !f.Name.EndsWith("_BackingField")  // Ignore backing fields for properties otherwise we report twice.
-				&& taskQueueBackgroundOperationManagerType.IsAssignableFrom(f.FieldType)))
-			{
-				value = null;
-				try
-				{
-					value = f.GetValue(this) as TaskQueueBackgroundOperationManager;
-				}
-				catch { }
-				if (null != value)
-					yield return value;
-			}
+			base.UninitializeApplication(vault);
 		}
 	}
 }
