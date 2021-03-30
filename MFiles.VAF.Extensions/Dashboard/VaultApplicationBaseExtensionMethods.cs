9da6632f76eec5b7f84336bee6024c8454819628
Aaron Hardin <aaron.hardin@m-files.com>
2021-03-30 17:02:01 -0500

    WIP: Initial version
    Adds a row with button to dashboard for each TaskQueueBackgroundOperation marked with particular attribute
    Button will trigger the operation using RunOnce

---
+++ MFiles.VAF.Extensions/Dashboard/VaultApplicationBaseExtensionMethods.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using MFiles.VAF.Configuration.Domain;
+using MFiles.VAF.Configuration.Interfaces.Domain;
+using MFilesAPI;
+
+namespace MFiles.VAF.Extensions.Dashboard
+{
+	public static class VaultApplicationBaseExtensionMethods
+	{
+		public static ConfigurationDomainCommand CreateConfigurationDomainCommand
+		(
+			this VaultApplicationBase vaultApplicationBase,
+			string commandId,
+			string extMethod,
+			params object[] additionalParameters
+		)
+		{
+			var commandParams = new List<object> { extMethod };
+			if( additionalParameters != null && additionalParameters.Length > 0 )
+			{
+				commandParams.AddRange( additionalParameters );
+			}
+			var extMethodCall = new VaultExtensionMethodCall
+			{
+				Method = $"{vaultApplicationBase.GetType().Namespace}.{nameof( MFEventHandlerType.MFEventHandlerVaultExtensionMethod )}",
+				Params = commandParams.ToArray()
+			};
+			var extMethodSrc = new VaultExtensionMethodSource { Read = extMethodCall };
+			var extMethodSrcDef = new MethodSourceDefinition( extMethodSrc );
+
+			return new ConfigurationDomainCommand
+			{
+				ID = commandId,
+				ExtensionMethod = extMethodSrcDef
+			};
+		}
+
+
+	}
+
+}
