f6ff5e9302b26bdcfb14ad0ca8f880ccf864ee92
Mirko Schimandl <mirko.schimandl@dmsfactory.com>
2021-09-02 11:48:40 +0200

    Added ability to remove background operations

---
+++ MFiles.VAF.Extensions/TaskQueueBackgroundOperations/TaskQueueBackgroundOperationManager/RemoveBackgroundOperation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MFiles.VAF;
+using MFiles.VAF.MultiserverMode;
+
+namespace MFiles.VAF.Extensions
+{
+	public partial class TaskQueueBackgroundOperationManager
+	{
+
+		/// <summary>
+		/// Removes a background operation by the name.
+		/// </summary>
+		/// <param name="name">The name of the operation.</param>
+		/// <returns><see langword="true"/>if the operation could be removed.</returns>
+		public bool RemoveBackgroundOperation
+		(
+			string name
+		)
+		{
+			lock (TaskQueueBackgroundOperationManager._lock)
+			{
+				if (this.BackgroundOperations.ContainsKey(name) == false)
+					throw new ArgumentException(
+						$"No background operation with the name {name} in queue {this.QueueId} exists.",
+						nameof(name));
+
+				// Remove it from the dictionary.
+				return this.BackgroundOperations.Remove(name);
+			}
+		}
+	}
+}
