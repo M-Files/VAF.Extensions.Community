44dbd01f7bb8fbf181caabceecb84a5b284ec0be
Craig Hawker <craig.hawker@m-files.com>
2021-09-15 14:54:25 +0100

    Added ITaskProcessingJob<TDirective>.CommitWithNoAction. Provides a shorthand for hybrid processors which may want to complete a job without providing any actions to run in the Commit phase.

--- MFiles.VAF.Extensions/ExtensionMethods/ITaskProcessingJobOfTExtensionMethods.cs
+++ MFiles.VAF.Extensions/ExtensionMethods/ITaskProcessingJobOfTExtensionMethods.cs
@@ -10,6 +10,35 @@ namespace MFiles.VAF.Extensions.ExtensionMethods
 	public static class ITaskProcessingJobOfTExtensionMethods
 	{
 		/// <summary>
+		/// An action which can be used with <see cref="ITaskProcessingJob{TDirective}.Commit(Action{MFilesAPI.Vault})"/>
+		/// to indicate that the job does not have anything to do in the commit action.
+		/// </summary>
+		private static Action<MFilesAPI.Vault> NoAction { get; } = (v) => { };
+
+		/// <summary>
+		/// Commits the job, but does not write anything to the vault.
+		/// </summary>
+		/// <typeparam name="TDirective">The type of directive this job accepts.</typeparam>
+		/// <param name="job">The job to commit.</param>
+		/// <remarks>
+		/// The default transaction mode is <see cref="TransactionMode.Hybrid"/>, which forces the task processor to call
+		/// <see cref="ITaskProcessingJob{TDirective}.Commit(Action{MFilesAPI.Vault})"/> prior to the processing completing.
+		/// In some situations the task processing may not need to actually make any updates to the server.  This extension
+		/// method is simply a shorthand for passing a lambda with no body to
+		/// <see cref="ITaskProcessingJob{TDirective}.Commit(Action{MFilesAPI.Vault})"/>.
+		/// </remarks>
+		public static void CommitWithNoAction<TDirective>(this ITaskProcessingJob<TDirective> job)
+			where TDirective : TaskDirective
+		{
+			// Sanity.
+			if (null == job)
+				throw new ArgumentNullException(nameof(job));
+
+			// No action in the commit phase.
+			job.Commit(ITaskProcessingJobOfTExtensionMethods.NoAction);
+		}
+
+		/// <summary>
 		/// Updates the in-progress status of the task.
 		/// </summary>
 		/// <typeparam name="TDirective">The type of directive this job accepts.</typeparam>
