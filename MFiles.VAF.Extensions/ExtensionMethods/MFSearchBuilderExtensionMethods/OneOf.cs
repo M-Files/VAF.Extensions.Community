de9b0bd3305e721e5d06cd9f63d4fc1fc1786d85
Craig Hawker <craig.hawker@m-files.com>
2021-10-21 16:04:48 +0100

    Comment fix on error text.

--- MFiles.VAF.Extensions/ExtensionMethods/MFSearchBuilderExtensionMethods/OneOf.cs
+++ MFiles.VAF.Extensions/ExtensionMethods/MFSearchBuilderExtensionMethods/OneOf.cs
@@ -93,7 +93,7 @@ namespace MFiles.VAF.Extensions
 							// If we have to resolve it and have no vault reference then die.
 							if (false == identifier.IsResolved)
 								if (null == searchBuilder?.Vault)
-									throw new InvalidOperationException($"The search builder's vault reference is null, so the identifier with alias/GUID {identifier.Alias} cannot be resolved.");
+									throw new InvalidOperationException($"The search builder or vault reference is null, so the identifier with alias/GUID {identifier.Alias} cannot be resolved.");
 							
 							// Resolve if needed.
 							return identifier
