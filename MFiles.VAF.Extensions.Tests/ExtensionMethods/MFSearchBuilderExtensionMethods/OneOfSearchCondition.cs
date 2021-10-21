825adbf4fd97aa73ed2444a0ef16d3f10931e914
Craig Hawker <craig.hawker@m-files.com>
2021-10-21 10:41:02 +0100

    Initial implementation for issue #33. Unit tests added, no practical testing yet.

---
+++ MFiles.VAF.Extensions.Tests/ExtensionMethods/MFSearchBuilderExtensionMethods/OneOfSearchCondition.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MFiles.VAF.Common;
+using MFilesAPI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MFilesAPI.Extensions;
+using System.Collections;
+
+namespace MFiles.VAF.Extensions.Tests.ExtensionMethods.MFSearchBuilderExtensionMethods
+{
+	[TestClass]
+	public class OneOfSearchCondition
+		: PropertyValueSearchConditionTestBase<int[]>
+	{
+		public OneOfSearchCondition()
+			: base(new[] { MFDataType.MFDatatypeLookup, MFDataType.MFDatatypeMultiSelectLookup })
+		{
+		}
+
+		/// <inherit />
+		protected override void AddSearchCondition
+			(
+			MFSearchBuilder mfSearchBuilder,
+			int propertyDef,
+			int[] value,
+			MFConditionType conditionType = MFConditionType.MFConditionTypeEqual,
+			MFParentChildBehavior parentChildBehavior = MFParentChildBehavior.MFParentChildBehaviorNone,
+			DataFunctionCall dataFunctionCall = null,
+			PropertyDefOrObjectTypes indirectionLevels = null
+			)
+		{
+			// Sanity.
+			if (null == mfSearchBuilder)
+				throw new ArgumentNullException(nameof(mfSearchBuilder));
+
+			// Use our extension method.
+			mfSearchBuilder.OneOf(propertyDef, value, parentChildBehavior, indirectionLevels);
+		}
+		
+		/// <summary>
+		/// Tests that calling
+		/// <see cref="VAF.Extensions.MFSearchBuilderExtensionMethods.Property(MFSearchBuilder, int, bool?, MFConditionType, MFParentChildBehavior, DataFunctionCall, PropertyDefOrObjectTypes)"/>
+		/// adds a valid search condition.
+		/// </summary>
+		[TestMethod]
+		[DataRow(PropertyValueSearchConditionTestBase.TestLookupPropertyId, new int[] { 1 }, MFConditionType.MFConditionTypeEqual, MFParentChildBehavior.MFParentChildBehaviorNone, (PropertyDefOrObjectTypes)null)]
+		[DataRow(PropertyValueSearchConditionTestBase.TestMultiSelectLookupPropertyId, new int[] { 1, 2, 3 }, MFConditionType.MFConditionTypeEqual, MFParentChildBehavior.MFParentChildBehaviorNone, (PropertyDefOrObjectTypes)null)]
+		public void SearchConditionIsCorrect
+			(
+			int propertyDef, 
+			int[] input,
+			MFConditionType conditionType,
+			MFParentChildBehavior parentChildBehavior,
+			PropertyDefOrObjectTypes indirectionLevels
+			)
+		{
+			base.AssertSearchConditionIsCorrect
+			(
+				propertyDef,
+				MFDataType.MFDatatypeMultiSelectLookup,
+				input,
+				conditionType,
+				parentChildBehavior,
+				indirectionLevels
+			);
+		}
+		protected override void AssertAddSearchCondition(int[] value)
+		{
+			// We will throw if we have no items, so we need to include at least one.
+			base.AssertAddSearchCondition(new int[] { 1 });
+		}
+
+		protected override void AssertValueIsCorrect(int[] expected, TypedValue actual)
+		{
+			// Default comparison doesn't work with the arrays we're using, so do it the hard way.
+
+			// Copy the lookup IDs into a collection.
+			var actualValues = new List<int>(actual?.GetValueAsLookups()?.Cast<Lookup>().Select(l => l.Item));
+
+			// Make sure we have the same data.
+			Assert.AreEqual(expected.Length, actualValues.Count);
+			for(var i=0; i<expected.Length; i++)
+			{
+				Assert.AreEqual(expected[i], actualValues[i]);
+			}
+		}
+
+	}
+}
---
+++ MFiles.VAF.Extensions/ExtensionMethods/MFSearchBuilderExtensionMethods/OneOf.cs
@@ -0,0 +1,176 @@
+﻿using MFiles.VAF.Common;
+using MFilesAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MFiles.VAF.Extensions;
+using MFiles.VAF.Configuration;
+
+namespace MFiles.VAF.Extensions
+{
+	/// <summary>
+	/// Extension methods for the <see cref="MFSearchBuilder"/> class.
+	/// </summary>
+	// ReSharper disable once InconsistentNaming
+	public static partial class MFSearchBuilderExtensionMethods
+	{
+		/// <summary>
+		/// Adds a <see cref="SearchCondition"/> to the collection to represent a "one of" search condition (https://developer.m-files.com/APIs/COM-API/Searching/SearchConditions/#executing-a-one-of-search).
+		/// </summary>
+		/// <param name="searchBuilder">The <see cref="MFSearchBuilder"/> to add the condition to.</param>
+		/// <param name="propertyDef">The ID of the property to search by.</param>
+		/// <param name="lookupIds">The ids to search for (the object must have the property set to one of these IDs).</param>
+		/// <param name="parentChildBehavior">Whether to accept matches to parent/child values as well (defaults to <see cref="MFParentChildBehavior.MFParentChildBehaviorNone"/>).</param>
+		/// <param name="indirectionLevels">The indirection levels (from the search object) to access the property to match.</param>
+		/// <returns>The <paramref name="searchBuilder"/> provided, for chaining.</returns>
+		public static MFSearchBuilder OneOf
+		(
+			this MFSearchBuilder searchBuilder,
+			int propertyDef,
+			IEnumerable<int> lookupIds,
+			MFParentChildBehavior parentChildBehavior = MFParentChildBehavior.MFParentChildBehaviorNone,
+			PropertyDefOrObjectTypes indirectionLevels = null
+		)
+		{
+			// Sanity.
+			if (null == searchBuilder)
+				throw new ArgumentNullException(nameof(searchBuilder));
+			if (0 > propertyDef)
+				throw new ArgumentOutOfRangeException(nameof(propertyDef), "Property Ids must be greater than -1; ensure that your property alias was resolved.");
+			if (null == lookupIds || false == lookupIds.Any())
+				throw new ArgumentException("The lookup collection does not contain any items.", nameof(lookupIds));
+
+			// What is the type of this property?
+			var dataType = searchBuilder.Vault.PropertyDefOperations.GetPropertyDef(propertyDef).DataType;
+
+			// If it is not valid then throw.
+			if (dataType != MFDataType.MFDatatypeLookup && dataType != MFDataType.MFDatatypeMultiSelectLookup)
+				throw new ArgumentException($"Property {propertyDef} is not a lookup or multi-select lookup property.", nameof(propertyDef));
+
+			// Add the search condition.
+			return searchBuilder.AddPropertyValueSearchCondition
+			(
+				propertyDef,
+				MFDataType.MFDatatypeMultiSelectLookup,
+				lookupIds.ToArray(),
+				MFConditionType.MFConditionTypeEqual,
+				parentChildBehavior,
+				indirectionLevels
+			);
+		}
+
+		/// <summary>
+		/// Adds a <see cref="SearchCondition"/> to the collection to represent a "one of" search condition (https://developer.m-files.com/APIs/COM-API/Searching/SearchConditions/#executing-a-one-of-search).
+		/// </summary>
+		/// <param name="searchBuilder">The <see cref="MFSearchBuilder"/> to add the condition to.</param>
+		/// <param name="propertyDef">The ID of the property to search by.</param>
+		/// <param name="identifiers">The items to search for (the object must have the property set to one of these).</param>
+		/// <param name="parentChildBehavior">Whether to accept matches to parent/child values as well (defaults to <see cref="MFParentChildBehavior.MFParentChildBehaviorNone"/>).</param>
+		/// <param name="indirectionLevels">The indirection levels (from the search object) to access the property to match.</param>
+		/// <returns>The <paramref name="searchBuilder"/> provided, for chaining.</returns>
+		public static MFSearchBuilder OneOf
+		(
+			this MFSearchBuilder searchBuilder,
+			int propertyDef,
+			IEnumerable<MFIdentifier> identifiers,
+			MFParentChildBehavior parentChildBehavior = MFParentChildBehavior.MFParentChildBehaviorNone,
+			PropertyDefOrObjectTypes indirectionLevels = null
+		)
+		{
+			// Use the other overload.
+			return searchBuilder.OneOf
+			(
+				propertyDef,
+				identifiers?.Select(identifier => identifier.ID),
+				parentChildBehavior,
+				indirectionLevels
+			);
+		}
+
+		/// <summary>
+		/// Adds a <see cref="SearchCondition"/> to the collection to represent a "one of" search condition (https://developer.m-files.com/APIs/COM-API/Searching/SearchConditions/#executing-a-one-of-search).
+		/// </summary>
+		/// <param name="searchBuilder">The <see cref="MFSearchBuilder"/> to add the condition to.</param>
+		/// <param name="propertyDef">The ID of the property to search by.</param>
+		/// <param name="objects">The items to search for (the object must have the property set to one of these).</param>
+		/// <param name="parentChildBehavior">Whether to accept matches to parent/child values as well (defaults to <see cref="MFParentChildBehavior.MFParentChildBehaviorNone"/>).</param>
+		/// <param name="indirectionLevels">The indirection levels (from the search object) to access the property to match.</param>
+		/// <returns>The <paramref name="searchBuilder"/> provided, for chaining.</returns>
+		public static MFSearchBuilder OneOf
+		(
+			this MFSearchBuilder searchBuilder,
+			int propertyDef,
+			IEnumerable<ObjVer> objects,
+			MFParentChildBehavior parentChildBehavior = MFParentChildBehavior.MFParentChildBehaviorNone,
+			PropertyDefOrObjectTypes indirectionLevels = null
+		)
+		{
+			// Use the other overload.
+			return searchBuilder.OneOf
+			(
+				propertyDef,
+				objects?.Select(objVer => objVer.ID),
+				parentChildBehavior,
+				indirectionLevels
+			);
+		}
+
+		/// <summary>
+		/// Adds a <see cref="SearchCondition"/> to the collection to represent a "one of" search condition (https://developer.m-files.com/APIs/COM-API/Searching/SearchConditions/#executing-a-one-of-search).
+		/// </summary>
+		/// <param name="searchBuilder">The <see cref="MFSearchBuilder"/> to add the condition to.</param>
+		/// <param name="propertyDef">The ID of the property to search by.</param>
+		/// <param name="objects">The items to search for (the object must have the property set to one of these).</param>
+		/// <param name="parentChildBehavior">Whether to accept matches to parent/child values as well (defaults to <see cref="MFParentChildBehavior.MFParentChildBehaviorNone"/>).</param>
+		/// <param name="indirectionLevels">The indirection levels (from the search object) to access the property to match.</param>
+		/// <returns>The <paramref name="searchBuilder"/> provided, for chaining.</returns>
+		public static MFSearchBuilder OneOf
+		(
+			this MFSearchBuilder searchBuilder,
+			int propertyDef,
+			IEnumerable<ObjID> objects,
+			MFParentChildBehavior parentChildBehavior = MFParentChildBehavior.MFParentChildBehaviorNone,
+			PropertyDefOrObjectTypes indirectionLevels = null
+		)
+		{
+			// Use the other overload.
+			return searchBuilder.OneOf
+			(
+				propertyDef,
+				objects?.Select(objID => objID.ID),
+				parentChildBehavior,
+				indirectionLevels
+			);
+		}
+
+		/// <summary>
+		/// Adds a <see cref="SearchCondition"/> to the collection to represent a "one of" search condition (https://developer.m-files.com/APIs/COM-API/Searching/SearchConditions/#executing-a-one-of-search).
+		/// </summary>
+		/// <param name="searchBuilder">The <see cref="MFSearchBuilder"/> to add the condition to.</param>
+		/// <param name="propertyDef">The ID of the property to search by.</param>
+		/// <param name="objects">The items to search for (the object must have the property set to one of these).</param>
+		/// <param name="parentChildBehavior">Whether to accept matches to parent/child values as well (defaults to <see cref="MFParentChildBehavior.MFParentChildBehaviorNone"/>).</param>
+		/// <param name="indirectionLevels">The indirection levels (from the search object) to access the property to match.</param>
+		/// <returns>The <paramref name="searchBuilder"/> provided, for chaining.</returns>
+		public static MFSearchBuilder OneOf
+		(
+			this MFSearchBuilder searchBuilder,
+			int propertyDef,
+			IEnumerable<ObjVerEx> objects,
+			MFParentChildBehavior parentChildBehavior = MFParentChildBehavior.MFParentChildBehaviorNone,
+			PropertyDefOrObjectTypes indirectionLevels = null
+		)
+		{
+			// Use the other overload.
+			return searchBuilder.OneOf
+			(
+				propertyDef,
+				objects?.Select(objVerEx => objVerEx.ID),
+				parentChildBehavior,
+				indirectionLevels
+			);
+		}
+	}
+}
