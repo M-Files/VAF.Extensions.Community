bbaa680076037d454f3884277588564a7275e8bd
Falk Huth <f.huth@ewerk.com>
2022-01-27 23:49:28 +0100

    added method CanCurrentUserChangePermissions.cs with test

---
+++ MFiles.VAF.Extensions.Tests/ExtensionMethods/ObjVerEx/CanCurrentUserChangePermissions.cs
@@ -0,0 +1,112 @@
+﻿using MFilesAPI;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+
+using System;
+
+namespace MFiles.VAF.Extensions.Tests.ExtensionMethods.ObjVerEx
+{
+	[TestClass]
+	public partial class CanCurrentUserChangePermissions
+		: TestBaseWithVaultMock
+	{
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void ThrowsIfNullObjVerEx()
+		{
+			SessionInfo sessionInfo = new SessionInfo();
+			((Common.ObjVerEx) null).CanCurrentUserChangePermissions(sessionInfo);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void ThrowsIfNullSessionInfo()
+		{
+			// Set up the vault mock.
+			var vaultMock = this.GetVaultMock();
+
+			// Set up the ObjVerEx.
+			var objVerEx = new Common.ObjVerEx
+			(
+				vaultMock.Object,
+				0,
+				1,
+				2
+			);
+
+			objVerEx.CanCurrentUserChangePermissions((SessionInfo)null);
+		}
+
+		[TestMethod]
+		public void CreateTestSessionInfo()
+		{
+			//SessionInfo sessionInfoFalse = new CanCurrentUserChangePermissionsSessionInfoTest(canCurrentUserChangePermissions: false);
+			SessionInfo sessionInfoFalse = new SessionInfo();
+			Assert.IsNotNull(sessionInfoFalse);
+		}
+
+		[TestMethod]
+		public void ReturnsTrueForDefaultObjVerEx()
+		{
+			// Set up our configuration.
+			var childObjectType = 102;
+
+			// Set up the object type operations mock.
+			var objectTypeOperationsMock = new Mock<VaultObjectTypeOperations>();
+			objectTypeOperationsMock
+				.Setup(m => m.GetObjectType(It.IsAny<int>()))
+				.Returns((int objectTypeId) =>
+				{
+					// Is it a child?
+					if (objectTypeId == childObjectType)
+					{
+						return new ObjType()
+						{
+							ID = childObjectType,
+							NamePlural = "Children",
+							NameSingular = "Child",
+							HasOwnerType = false
+						};
+					}
+
+					// Unexpected object type.
+					throw new InvalidOperationException("Unexpected object type");
+				});
+
+			// Set up the vault mock.
+			var vaultMock = this.GetVaultMock();
+			vaultMock.Setup(v => v.ObjectTypeOperations).Returns(objectTypeOperationsMock.Object);
+
+			// Set up the ObjVerEx.
+			var objVerEx = new Common.ObjVerEx
+			(
+				vaultMock.Object,
+				childObjectType,
+				1,
+				2
+			);
+
+			// Set up the session info objects
+			//SessionInfo sessionInfoTrue = new CanCurrentUserChangePermissionsSessionInfoTest(canCurrentUserChangePermissions: true);
+			//SessionInfo sessionInfoFalse = new CanCurrentUserChangePermissionsSessionInfoTest(canCurrentUserChangePermissions: false);
+			SessionInfo sessionInfoTrue = new SessionInfo();
+			SessionInfo sessionInfoFalse = new SessionInfo();
+
+			// TODO: Mock SessionInfo, objVerEx.ACL and objVerEx.Permissions to set it to the right result here
+
+			// Return true if the method returns true
+			Assert.IsTrue(
+				objVerEx.CanCurrentUserChangePermissions(sessionInfoTrue)
+			);
+
+			// Return false if the method returns false
+			Assert.IsFalse(
+				objVerEx.CanCurrentUserChangePermissions(sessionInfoFalse)
+			);
+		}
+
+	}
+}
---
+++ MFiles.VAF.Extensions/ExtensionMethods/ObjVerEx/CanCurrentUserChangePermissions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MFiles.VAF.Common;
+
+using MFilesAPI;
+
+namespace MFiles.VAF.Extensions
+{
+	public static partial class ObjVerExExtensionMethods
+	{
+		/// <summary>
+		/// Indicates whether the passed user can change permissions this object version.
+		/// </summary>
+		/// <param name="objVerEx">The ObjVerEx object.</param>
+		/// <param name="sessionInfo">The session to check access for.</param>
+		/// <returns>True if the user can change permissions this version, otherwise False.</returns>
+		public static bool CanCurrentUserChangePermissions
+		(
+			this ObjVerEx objVerEx,
+			SessionInfo sessionInfo
+		)
+		{
+			// Sanity.
+			if (null == objVerEx)
+				throw new ArgumentNullException(nameof(objVerEx));
+			if (sessionInfo is null)
+				throw new ArgumentNullException(nameof(sessionInfo));
+
+			return sessionInfo.CheckObjectAccess(objVerEx.ACL, MFObjectAccess.MFObjectAccessChangePermissions);
+		}
+	}
+}
