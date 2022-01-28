617a6eb5594660fe4952b7890def36a2486c3d13
Craig Hawker <craig.hawker@m-files.com>
2022-01-28 16:51:55 +0000

    Made ObjVerExLogSensitivityFilter return ID data if no filter found for ObjectVersion.

--- MFiles.VAF.Extensions/Logging/Sensitivity/Filters/ObjVerExLogSensitivityFilter.cs
+++ MFiles.VAF.Extensions/Logging/Sensitivity/Filters/ObjVerExLogSensitivityFilter.cs
@@ -1,4 +1,5 @@
 ﻿using MFiles.VAF.Common;
+using MFiles.VaultApplications.Logging;
 using MFiles.VaultApplications.Logging.Sensitivity;
 using MFilesAPI;
 using System;
@@ -15,6 +16,8 @@ namespace MFiles.VAF.Extensions.Logging.Sensitivity.Filters
 	public class ObjVerExLogSensitivityFilter
 		: LogSensitivityFilterBase<ObjVerEx>
 	{
+		private ILogger Logger { get; } = LogManager.GetLogger(typeof(ObjVerExLogSensitivityFilter));
+
 		/// <inheritdoc />
 		public override string FilterValueForLogging(ObjVerEx value, VaultApplications.Logging.Sensitivity.Sensitivity level, string format, IFormatProvider formatProvider)
 		{
@@ -23,11 +26,12 @@ namespace MFiles.VAF.Extensions.Logging.Sensitivity.Filters
 				return String.Empty;
 
 			// Try and get an ObjectVersion filter from the registered factories.
-			if (this.TryGetFilterFromFactories<ObjectVersion>(out ILogSensitivityFilter filter))
+			if (this.TryGetFilter<ObjectVersion>(out ILogSensitivityFilter filter))
 				return filter?.FilterValueForLogging(value, level, format, formatProvider);
 
 			// No suitable filter.
-			return String.Empty;
+			this.Logger.Error($"Could not load required sensitivity filter for {typeof(ObjectVersion).FullName}.  Minimum data will be logged.");
+			return $"{value.ObjVer.Type}-{value.ObjVer.ID}-{value.ObjVer.Version}";
 		}
 	}
 }
