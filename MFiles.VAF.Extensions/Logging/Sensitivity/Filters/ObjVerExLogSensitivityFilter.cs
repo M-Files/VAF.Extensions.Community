a58152be9b9c4cba66c42c23b3fd0c08fe90e190
Craig Hawker <craig.hawker@m-files.com>
2022-02-11 15:38:55 +0000

    Update to latest logging framework.

--- MFiles.VAF.Extensions/Logging/Sensitivity/Filters/ObjVerExLogSensitivityFilter.cs
+++ MFiles.VAF.Extensions/Logging/Sensitivity/Filters/ObjVerExLogSensitivityFilter.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 // By placing this in the MFiles.VaultApplications.Logging namespace, the "RenderInternalLogs" will apply to this too.
-namespace MFiles.VaultApplications.Logging.Sensitivity
+namespace MFiles.VaultApplications.Logging.Sensitivity.Filters
 {
 	/// <summary>
 	/// Implements a log sensitivity filter for <see cref="ObjVerEx>"/>.
@@ -18,19 +18,20 @@ namespace MFiles.VaultApplications.Logging.Sensitivity
 		private ILogger Logger { get; } = LogManager.GetLogger(typeof(ObjVerExLogSensitivityFilter));
 
 		/// <inheritdoc />
-		public override string FilterValueForLogging(ObjVerEx value, VaultApplications.Logging.Sensitivity.Sensitivity level, IEnumerable<string> customFlags, string format, IFormatProvider formatProvider)
+		public override string FilterValueForLogging(ObjVerEx value, Sensitivity level, IEnumerable<string> customFlags, string format, IFormatProvider formatProvider)
 		{
 			// Sanity.
 			if (null == value?.Info)
 				return String.Empty;
 
 			// Try and get an ObjectVersion filter from the registered factories.
-			if (this.TryGetFilter<ObjectVersion>(out ILogSensitivityFilter filter))
-				return filter?.FilterValueForLogging(value, level, customFlags, format, formatProvider);
+			if (false == this.TryGetFilter<IObjectVersion>(out ILogSensitivityFilter<IObjectVersion> filter) || null == filter)
+			{
+				this.Logger?.Error($"Could not load required sensitivity filter for {typeof(IObjectVersion).FullName}.  {typeof(ObjectVersionLogSensitivityFilter).FullName} will be used.");
+				filter = new ObjectVersionLogSensitivityFilter();
+			}
 
-			// No suitable filter.
-			this.Logger?.Error($"Could not load required sensitivity filter for {typeof(ObjectVersion).FullName}.  Minimum data will be logged.");
-			return $"{value.ObjVer.Type}-{value.ObjVer.ID}-{value.ObjVer.Version}";
+			return filter.FilterValueForLogging(value.Info, level, customFlags, format, formatProvider);
 		}
 	}
 }
