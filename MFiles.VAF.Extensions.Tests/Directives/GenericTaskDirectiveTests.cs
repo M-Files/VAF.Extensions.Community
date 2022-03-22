d19351e103837282f3aba4142573057299f9f0dc
Simo Kivistö <simo.kivisto@m-files.com>
2022-03-22 11:12:10 +0200

    Add GenericTaskDirective with 1 to 3 type parameters

---
+++ MFiles.VAF.Extensions.Tests/Directives/GenericTaskDirectiveTests.cs
@@ -0,0 +1,99 @@
+﻿using MFilesAPI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace MFiles.VAF.Extensions.Tests.Directives
+{
+	[TestClass]
+	public class GenericTaskDirectiveTests
+		: TaskDirectiveWithDisplayNameTestsBase<ObjIDTaskDirective>
+	{
+		[TestMethod]
+		public void ValueIsReadWrite()
+		{
+			var type = typeof(GenericTaskDirective<int>);
+			var property = type.GetProperty(nameof(GenericTaskDirective<int>.Value));
+			Assert.IsNotNull(property);
+			Assert.IsTrue(property.CanRead);
+			Assert.IsTrue(property.CanWrite);
+		}
+
+		[TestMethod]
+		public void Value2IsReadWrite()
+		{
+			var type = typeof(GenericTaskDirective<int, int>);
+			var property = type.GetProperty(nameof(GenericTaskDirective<int, int>.Value2));
+			Assert.IsNotNull(property);
+			Assert.IsTrue(property.CanRead);
+			Assert.IsTrue(property.CanWrite);
+		}
+
+		[TestMethod]
+		public void Value3IsReadWrite()
+		{
+			var type = typeof(GenericTaskDirective<int, int, int>);
+			var property = type.GetProperty(nameof(GenericTaskDirective<int, int, int>.Value3));
+			Assert.IsNotNull(property);
+			Assert.IsTrue(property.CanRead);
+			Assert.IsTrue(property.CanWrite);
+		}
+
+		[TestMethod]
+		public void GenericDirective1CorrectDataInt()
+		{
+			var genericDirective = new GenericTaskDirective<int>(1);
+			Assert.AreEqual(1, genericDirective.Value);
+		}
+
+		[TestMethod]
+		public void GenericDirective2CorrectDataInt()
+		{
+			var genericDirective = new GenericTaskDirective<int, int>(1, 2);
+			Assert.AreEqual(1, genericDirective.Value);
+			Assert.AreEqual(2, genericDirective.Value2);
+		}
+
+		[TestMethod]
+		public void GenericDirective3CorrectDataInt()
+		{
+			var genericDirective = new GenericTaskDirective<int, int, int>(1, 2, 3);
+			Assert.AreEqual(1, genericDirective.Value);
+			Assert.AreEqual(2, genericDirective.Value2);
+			Assert.AreEqual(3, genericDirective.Value3);
+		}
+
+		[TestMethod]
+		public void GenericDirective1CorrectDataString()
+		{
+			var genericDirective = new GenericTaskDirective<string>("value1");
+			Assert.AreEqual("value1", genericDirective.Value);
+		}
+
+		[TestMethod]
+		public void GenericDirective2CorrectDataString()
+		{
+			var genericDirective = new GenericTaskDirective<string, string>("value1", "value2");
+			Assert.AreEqual("value1", genericDirective.Value);
+			Assert.AreEqual("value2", genericDirective.Value2);
+		}
+
+		[TestMethod]
+		public void GenericDirective3CorrectDataString()
+		{
+			var genericDirective = new GenericTaskDirective<string, string, string>("value1", "value2", "value3");
+			Assert.AreEqual("value1", genericDirective.Value);
+			Assert.AreEqual("value2", genericDirective.Value2);
+			Assert.AreEqual("value3", genericDirective.Value3);
+		}
+
+		[TestMethod]
+		public void GenericDirective1CorrectDataList()
+		{
+			var genericDirective = new GenericTaskDirective<IList<int>>(new List<int> { 1, 2, 3 });
+			Assert.AreEqual(1, genericDirective.Value[0]);
+			Assert.AreEqual(2, genericDirective.Value[1]);
+			Assert.AreEqual(3, genericDirective.Value[2]);
+		}
+	}
+}
---
+++ MFiles.VAF.Extensions/Directives/GenericTaskDirective.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFiles.VAF.Extensions
+{
+	[DataContract]
+	public class GenericTaskDirective<T> : TaskDirectiveWithDisplayName
+	{
+		public GenericTaskDirective(T value, string displayName = null)
+		{
+			this.DisplayName = displayName;
+			this.Value = value;
+		}
+
+		[DataMember]
+		public T Value { get; set; }
+	}
+
+	[DataContract]
+	public class GenericTaskDirective<T, Y> : GenericTaskDirective<T>
+	{
+		public GenericTaskDirective(T value, Y value2, string displayName = null) : base(value, displayName)
+		{
+			this.Value2 = value2;
+		}
+
+		[DataMember]
+		public Y Value2 { get; set; }
+	}
+
+	[DataContract]
+	public class GenericTaskDirective<T, Y, Z> : GenericTaskDirective<T, Y>
+	{
+		public GenericTaskDirective(T value, Y value2, Z value3, string displayName = null) : base(value, value2, displayName)
+		{
+			this.Value3 = value3;
+		}
+
+		[DataMember]
+		public Z Value3 { get; set; }
+	}
+}
