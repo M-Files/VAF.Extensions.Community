ed3e97318ea5b793472fa0f45a7faa900bb59dc2
Craig Hawker <craig.hawker@m-files.com>
2022-04-28 14:39:28 +0100

    Include validation of log download path. Fix for issue #61

--- MFiles.VAF.Extensions/Dashboards/Commands/DefaultLogDashboardCommandBase.cs
+++ MFiles.VAF.Extensions/Dashboards/Commands/DefaultLogDashboardCommandBase.cs
@@ -1,8 +1,11 @@
-﻿using MFiles.VAF.Configuration.AdminConfigurations;
+﻿using MFiles.VAF.Common;
+using MFiles.VAF.Configuration.AdminConfigurations;
+using MFilesAPI;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
 
 namespace MFiles.VAF.Extensions.Dashboards.Commands
 {
@@ -30,34 +33,56 @@ namespace MFiles.VAF.Extensions.Dashboards.Commands
 
 		/// <summary>
 		/// Helper method.
-		/// Returns the applications default logging dir, as defined by the environment.
+		/// Returns the application's default logging dir, as defined by the environment.
 		/// </summary>
+		/// <param name="vault">The vault used for verifying valid root paths in some cases.</param>
 		/// <returns>The logging path.</returns>
-		public string GetDefaultLogPath()
-			=> MFiles.VaultApplications.Logging.NLog.Targets.DefaultTargetConfiguration.GetDefaultTargetLoggingDirectory();
-
-		/// <summary>
-		/// Gets the root path for all logs of this application,
-		/// not just the current server and numbered folder.
-		/// </summary>
-		/// <returns>The root path.</returns>
-		public string GetRootLogPathForAllInstances()
+		internal static string ResolveRootLogPath(Vault vault)
 		{
-			// Go back two levels.
-			// 1st level: Running number - in case of folder locks on startup
-			// 2nd level: Server Instance Id
-			return Path.GetFullPath(Path.Combine(this.GetDefaultLogPath(), "..//.."));
+			// If the root path is specified as an environment variable, use it and be done.
+			string envRootPath = Environment.GetEnvironmentVariable(
+					"MFAPPLOGDIRROOT", EnvironmentVariableTarget.Process);
+			if (!string.IsNullOrWhiteSpace(envRootPath))
+				return envRootPath;
+
+			// The root path was not specified.
+			// Try to infer it from the current log dir.
+			string envLogPath = Environment.GetEnvironmentVariable("MFAPPLOGDIR", EnvironmentVariableTarget.Process);
+			if (!string.IsNullOrWhiteSpace(envLogPath) && Path.IsPathRooted(envLogPath))
+			{
+				// Verfiy the log path matches our assumptions or else it is too dangerous to infer the root from.
+				// We assume the path ends with "\Applogs\<app-guid>\<instance-guid>\<number>\".
+				// Guids are in uppercase with dashes and no curly braces.
+				string appGuid = ApplicationDefinition.Guid.ToString("D").ToUpper();
+				string instanceGuid = Guid.Parse(input: vault.GetVaultServerAttachments().GetCurrent().ServerID)
+						.ToString("D").ToUpper();
+				var regex = new Regex($@"\\Applogs\\{appGuid}\\{instanceGuid}\\\d+\\$");
+				if (regex.IsMatch(envLogPath))
+				{
+					// Path matches our assumptions.
+					// Return the root path as the appGuid directory (2 levels back).
+					return Path.GetFullPath(Path.Combine(envLogPath, "..//.."));
+				}
+			}
+
+			// No valid root path was found.
+			return null;
 		}
 
 		/// <summary>
 		/// Provides a list of all log files and their sizes relative to the root log path.
 		/// </summary>
 		/// <returns>File list.</returns>
-		public List<LogFileInfo> ResolveLogFiles()
+		public List<LogFileInfo> ResolveLogFiles(Vault vault)
 		{
+			var rootPath = ResolveRootLogPath(vault);
+
+			// Sanity check.
+			if (string.IsNullOrWhiteSpace(rootPath))
+				throw new Exception("The server environment does not support downloading application logs.");
+
 			// Collect all log files.
 			var files = new List<LogFileInfo>();
-			var rootPath = GetRootLogPathForAllInstances();
 			foreach (string filePath in Directory.GetFiles(rootPath, "*.log", SearchOption.AllDirectories))
 			{
 				// Include info about this log file.
@@ -72,5 +97,7 @@ namespace MFiles.VAF.Extensions.Dashboards.Commands
 			}
 			return files;
 		}
+
+
 	}
 }
