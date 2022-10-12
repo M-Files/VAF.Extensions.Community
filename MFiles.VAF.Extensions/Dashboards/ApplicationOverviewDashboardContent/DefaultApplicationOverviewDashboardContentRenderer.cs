a972fdb16d0c35520fad91d370a98f3cf028c6a0
Craig Hawker <craig.hawker@m-files.com>
2022-10-12 09:25:01 +0100

    Better descriptions for licensing status. Moved application details strings to resources.

--- MFiles.VAF.Extensions/Dashboards/ApplicationOverviewDashboardContent/DefaultApplicationOverviewDashboardContentRenderer.cs
+++ MFiles.VAF.Extensions/Dashboards/ApplicationOverviewDashboardContent/DefaultApplicationOverviewDashboardContentRenderer.cs
@@ -1,4 +1,5 @@
 ﻿using MFiles.VAF.Common;
+using MFiles.VAF.Configuration;
 using MFiles.VAF.Configuration.Domain.Dashboards;
 using MFiles.VAF.Extensions.ExtensionMethods;
 using System;
@@ -15,7 +16,7 @@ namespace MFiles.VAF.Extensions.Dashboards.ApplicationOverviewDashboardContent
 		where TSecureConfiguration : class, new()
 	{ 
 		/// <inheritdoc />
-		public string Title { get; set; } = "Application Details";
+		public string Title { get; set; } = Resources.Licensing.ApplicationDetailsPanel_Title;
 
 		/// <inheritdoc />
 		public bool ShowVersion { get; set; } = true;
@@ -44,11 +45,39 @@ namespace MFiles.VAF.Extensions.Dashboards.ApplicationOverviewDashboardContent
 		}
 
 		/// <summary>
-		/// Generates the content for the "application overview" dashboard section.
+		/// Generates the content for the "application details" and, if appropriate, "application licensing" dashboard sections.
 		/// </summary>
 		/// <returns>The content, or null to render nothing.</returns>
-		public virtual DashboardPanelEx GetDashboardContent()
+		public virtual IDashboardContent GetDashboardContent()
 		{
+			var content = new DashboardContentCollection();
+
+			// Add the details panel, if one is returned.
+			{
+				var detailsPanel = this.GetApplicationDetailsDashboardContent();
+				if (null != detailsPanel)
+					content.Add(detailsPanel);
+			}
+
+			// TODO: Add a licensing panel, if one is returned.
+			{
+				if (this.ShowLicenseStatus)
+				{
+
+				}
+			}
+
+			// Return all the content.
+			return content;
+
+		}
+
+		/// <summary>
+		/// Generates the "Application Details" dashboard content.
+		/// </summary>
+		/// <returns></returns>
+		public virtual DashboardPanelEx GetApplicationDetailsDashboardContent()
+		{ 
 			var innerContent = new DashboardContentCollection();
 
 			var table = new DashboardTable();
@@ -58,21 +87,28 @@ namespace MFiles.VAF.Extensions.Dashboards.ApplicationOverviewDashboardContent
 			if (this.ShowVersion)
 			{
 				var row = table.AddRow();
-				row.AddCell("Version:", DashboardTableCellType.Header).HeaderStyles.Remove("border-bottom");
-				row.AddCell(ApplicationDefinition.Version.ToString()).Styles.Add("width", "100%");
+				row.AddCell(Resources.Licensing.ApplicationDetailsPanel_RowHeaders_Version, DashboardTableCellType.Header)
+					.HeaderStyles
+					.Remove("border-bottom");
+				row.AddCell(ApplicationDefinition.Version.ToString())
+					.Styles.Add("width", "100%");
 			}
 
 			// Add the publisher and copyright if we have them.
 			if (this.ShowPublisher && false == string.IsNullOrWhiteSpace(ApplicationDefinition.Publisher))
 			{
 				var row = table.AddRow();
-				row.AddCell("Publisher:", DashboardTableCellType.Header).HeaderStyles.Remove("border-bottom");
+				row.AddCell(Resources.Licensing.ApplicationDetailsPanel_RowHeaders_Publisher, DashboardTableCellType.Header)
+					.HeaderStyles
+					.Remove("border-bottom");
 				row.AddCell(ApplicationDefinition.Publisher.ToString());
 			}
 			if (this.ShowCopyright && false == string.IsNullOrWhiteSpace(ApplicationDefinition.Copyright))
 			{
 				var row = table.AddRow();
-				row.AddCell("Copyright:", DashboardTableCellType.Header).HeaderStyles.Remove("border-bottom");
+				row.AddCell(Resources.Licensing.ApplicationDetailsPanel_RowHeaders_Copyright, DashboardTableCellType.Header)
+					.HeaderStyles
+					.Remove("border-bottom");
 				row.AddCell($"&copy; {ApplicationDefinition.Copyright}");
 			}
 
@@ -83,11 +119,11 @@ namespace MFiles.VAF.Extensions.Dashboards.ApplicationOverviewDashboardContent
 			{
 				var element =
 					ApplicationDefinition.MultiServerCompatible
-					? new DashboardCustomContentEx($"<p style='color: green'>This application is marked as compatible with M-Files Multi-Server Mode.</p>")
+					? new DashboardCustomContentEx($"<p style='color: green'>{Resources.Licensing.ApplicationDetailsPanel_ApplicationIsMultiServerModeCompatible.EscapeXmlForDashboard()}</p>")
 					{
 						Icon = "Resources/Images/Completed.png"
 					}
-					: new DashboardCustomContentEx($"<p style='color: red'>This application is <strong>NOT</strong> marked as compatible with M-Files Multi-Server Mode.</p>")
+					: new DashboardCustomContentEx($"<p style='color: red'>{Resources.Licensing.ApplicationDetailsPanel_ApplicationIsNotMultiServerModeCompatible.EscapeXmlForDashboard()}</p>")
 					{
 						Icon = "Resources/Images/canceled.png"
 					};
@@ -100,24 +136,70 @@ namespace MFiles.VAF.Extensions.Dashboards.ApplicationOverviewDashboardContent
 			{
 				if (null != this.VaultApplication?.License)
 				{
-					string colour = null, icon = null;
-					switch(this.VaultApplication.License.LicenseStatus)
+					string colour = null, icon = null, status = null;
+					switch (this.VaultApplication.License.LicenseStatus)
 					{
+						// OKAY STATES
+
 						case MFilesAPI.MFApplicationLicenseStatus.MFApplicationLicenseStatusValid:
 							colour = "green";
 							icon = "Resources/Images/Completed.png";
+							status = Resources.Licensing.MFApplicationLicenseStatusValid;
+							break;
+						case MFilesAPI.MFApplicationLicenseStatus.MFApplicationLicenseStatusNotNeeded:
+							status = Resources.Licensing.MFApplicationLicenseStatusNotNeeded;
+							colour = "green";
+							icon = "Resources/Images/Completed.png";
+							break;
+						case MFilesAPI.MFApplicationLicenseStatus.MFApplicationLicenseStatusTrial:
+							status = Resources.Licensing.MFApplicationLicenseStatusTrial;
+							colour = "green";
+							icon = "Resources/Images/Completed.png";
+							break;
+						case MFilesAPI.MFApplicationLicenseStatus.MFApplicationLicenseStatusInstalled:
+							status = Resources.Licensing.MFApplicationLicenseStatusInstalled;
+							colour = "green";
+							icon = "Resources/Images/Completed.png";
+							break;
+
+						// WARNING STATES
+
+						case MFilesAPI.MFApplicationLicenseStatus.MFApplicationLicenseStatusUnknown:
+							status = Resources.Licensing.MFApplicationLicenseStatusUnknown;
+							colour = "darkorange";
+							icon = "Resources/Images/Failed.png";
+							break;
+						case MFilesAPI.MFApplicationLicenseStatus.MFApplicationLicenseStatusGracePeriod:
+							status = Resources.Licensing.MFApplicationLicenseStatusGracePeriod;
+							colour = "darkorange";
+							icon = "Resources/Images/Failed.png";
+							break;
+
+						// ERROR STATES
+
+						case MFilesAPI.MFApplicationLicenseStatus.MFApplicationLicenseStatusNotInstalled:
+							status = Resources.Licensing.MFApplicationLicenseStatusNotInstalled;
+							colour = "red";
+							icon = "Resources/Images/Failed.png";
 							break;
 						case MFilesAPI.MFApplicationLicenseStatus.MFApplicationLicenseStatusInvalid:
+							status = Resources.Licensing.MFApplicationLicenseStatusInvalid;
+							colour = "red";
+							icon = "Resources/Images/Failed.png";
+							break;
 						case MFilesAPI.MFApplicationLicenseStatus.MFApplicationLicenseStatusFormatError:
-						case MFilesAPI.MFApplicationLicenseStatus.MFApplicationLicenseStatusNotInstalled:
+							status = Resources.Licensing.MFApplicationLicenseStatusFormatError;
 							colour = "red";
 							icon = "Resources/Images/Failed.png";
 							break;
 					}
-					innerContent.Add(new DashboardCustomContentEx($"<p style='color: {colour}'><strong>License:</strong> {this.VaultApplication.GetApplicationLicenseDetails(false, null)}</p>")
-					{
-						Icon = icon
-					});
+
+					// Add the status if we can.
+					if (null != status)
+						innerContent.Add(new DashboardCustomContentEx($"<p style='color: {colour}'>{status}</p>")
+						{
+							Icon = icon
+						});
 				}
 			}
 
