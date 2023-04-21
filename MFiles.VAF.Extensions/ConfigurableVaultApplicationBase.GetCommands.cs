19f9eb7f748944a2d6fe0f91628b498d41c30cea
Craig Hawker <craig.hawker@m-files.com>
2023-04-21 11:57:53 +0100

    Split AggregatedCustomDomainCommandResolver to separate file. Added DefaultCustomDomainCommandResolver<T>. Moved the resolving of all custom domain commands - including task queues and the like - to the (new) default generic custom domain command resolver.

--- MFiles.VAF.Extensions/ConfigurableVaultApplicationBase.GetCommands.cs
+++ MFiles.VAF.Extensions/ConfigurableVaultApplicationBase.GetCommands.cs
@@ -1,4 +1,5 @@
 ﻿using MFiles.VAF.Configuration.AdminConfigurations;
+using MFiles.VAF.Extensions.Dashboards.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,21 +16,15 @@ namespace MFiles.VAF.Extensions
 			foreach (var c in base.GetCommands(context)?.AsNotNull())
 				yield return c;
 
-			// Return the command related to the background operation approach.
-			foreach (var c in this.GetTaskQueueBackgroundOperationRunCommands()?.AsNotNull())
-				yield return c;
-
-			// Return the commands related to the VAF 2.3+ attribute-based approach.
-			foreach (var c in this.TaskManager?.GetTaskQueueRunCommands(this.TaskQueueResolver)?.AsNotNull())
-				yield return c;
-
-			// Return the commands associated with downloading logs from the default file target.
-			foreach (var c in this.GetDefaultLogTargetDownloadCommands()?.AsNotNull())
-				yield return c;
-
-			// Return the commands declared via attributes.
-			foreach (var c in this.GetCustomDomainCommandResolver()?.GetCustomDomainCommands()?.AsNotNull())
-				yield return c;
+			// Return any commands that the resolver provides.
+			{
+				var resolver = this.GetCustomDomainCommandResolver();
+				if (resolver != null)
+				{
+					foreach (var c in resolver.GetCustomDomainCommands()?.AsNotNull())
+						yield return c;
+				}
+			}
 		}
 
 		/// <summary>
@@ -40,43 +35,7 @@ namespace MFiles.VAF.Extensions
 		/// <remarks>Returns <see cref="DefaultCustomDomainCommandResolver"/> by default.</remarks>
 		public virtual ICustomDomainCommandResolver GetCustomDomainCommandResolver()
 		{
-			var resolver = new DefaultCustomDomainCommandResolver();
-			resolver.Include(this);
-			return resolver;
-		}
-		
-		/// <summary>
-		/// Returns the commands associated with downloading logs from the default file target.
-		/// </summary>
-		/// <returns></returns>
-		public virtual IEnumerable<CustomDomainCommand> GetDefaultLogTargetDownloadCommands()
-		{
-			// One to allow them to select which logs...
-			yield return Dashboards.Commands.ShowSelectLogDownloadDashboardCommand.Create();
-
-			// ...and one that actually does the collation/download.
-			yield return Dashboards.Commands.DownloadSelectedLogsDashboardCommand.Create();
-			
-			// Allow the user to see the latest log entries.
-			yield return Dashboards.Commands.ShowLatestLogEntriesDashboardCommand.Create();
-			yield return Dashboards.Commands.RetrieveLatestLogEntriesCommand.Create();
-		}
-
-		/// <summary>
-		/// Returns the commands associated with manually running task queue background operations.
-		/// </summary>
-		/// <returns></returns>
-		public virtual IEnumerable<CustomDomainCommand> GetTaskQueueBackgroundOperationRunCommands()
-		{
-			// Get the background operations that have a run command.
-			// Note: this should be all of them.
-			return this
-				.GetType()
-				.GetPropertiesAndFieldsOfType<TaskQueueBackgroundOperationManager<TSecureConfiguration>>(this)
-				.SelectMany(tqbom => tqbom.BackgroundOperations)
-				.AsEnumerable()
-				.Select(bo => bo.Value?.DashboardRunCommand)
-				.Where(c => null != c);
+			return new DefaultCustomDomainCommandResolver<TSecureConfiguration>(this);
 		}
 	}
 }
---
+++ MFiles.VAF.Extensions/Dashboards/Commands/AggregatedCustomDomainCommandResolver.cs
@@ -0,0 +1,31 @@
+﻿using MFiles.VAF.Configuration.AdminConfigurations;
+using MFiles.VAF.Configuration.Domain.Dashboards;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MFiles.VAF.Extensions.Dashboards.Commands
+{
+	/// <summary>
+	/// An implementation of <see cref="ICustomDomainCommandResolver"/> that 
+	/// can be used to return custom domain commands from multiple other instances
+	/// of <see cref="ICustomDomainCommandResolver"/>.
+	/// </summary>
+	public class AggregatedCustomDomainCommandResolver
+		: ICustomDomainCommandResolver
+	{
+		public List<ICustomDomainCommandResolver> CustomDomainCommandResolvers { get; }
+			= new List<ICustomDomainCommandResolver>();
+
+		/// <inheritdoc />
+		public virtual IEnumerable<CustomDomainCommand> GetCustomDomainCommands()
+			=> CustomDomainCommandResolvers?
+				.SelectMany(r => r.GetCustomDomainCommands()?.AsNotNull())?
+				.AsNotNull();
+
+		/// <inheritdoc />
+		public virtual DashboardDomainCommandEx GetDashboardDomainCommand(string commandId, DashboardCommandStyle style = DashboardCommandStyle.Link)
+			=> CustomDomainCommandResolvers?
+			.Select(c => c.GetDashboardDomainCommand(commandId, style))
+			.FirstOrDefault(c => c != null);
+	}
+}
