beff92a78aeab845dd3c1e35b94f246bd0691040
Craig Hawker <craig.hawker@m-files.com>
2023-05-31 09:06:00 +0100

    Fixed namespace.

--- MFiles.VAF.Extensions/ExtensionMethods/ObjVerEx/IsEnteringState.cs
+++ MFiles.VAF.Extensions/ExtensionMethods/ObjVerEx/IsEnteringState.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MFiles.VAF.Extensions.ExtensionMethods
+namespace MFiles.VAF.Extensions
 {
 	public static partial class ObjVerExExtensionMethods
 	{
