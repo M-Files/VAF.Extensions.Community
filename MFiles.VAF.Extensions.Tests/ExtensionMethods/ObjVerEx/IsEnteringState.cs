945d882b0fecd033bc3c9bd82367e4f26c7b7b53
Craig Hawker <craig.hawker@m-files.com>
2023-05-31 09:20:52 +0100

    Added some really basic unit tests.

---
+++ MFiles.VAF.Extensions.Tests/ExtensionMethods/ObjVerEx/IsEnteringState.cs
@@ -0,0 +1,29 @@
+﻿using MFiles.VAF.Configuration;
+using MFilesAPI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace MFiles.VAF.Extensions.Tests.ExtensionMethods.ObjVerEx
+{
+	[TestClass]
+	public class IsEnteringState
+		: TestBaseWithVaultMock
+	{
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void ThrowsIfNullObjVerEx()
+		{
+			((Common.ObjVerEx)null).IsEnteringState(123);
+		}
+
+		[TestMethod]
+		public void ReturnsFalseIfNullMFIdentifier()
+		{
+			Assert.IsFalse(new MFiles.VAF.Common.ObjVerEx().IsEnteringState(123));
+		}
+
+
+	}
+}
