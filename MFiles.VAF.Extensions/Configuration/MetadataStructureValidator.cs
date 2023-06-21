90e5012b32021a2e3da1d6d2c153fe6bb46a201c
Craig Hawker <craig.hawker@m-files.com>
2023-06-21 10:50:40 +0100

    More logging added.

--- MFiles.VAF.Extensions/Configuration/MetadataStructureValidator.cs
+++ MFiles.VAF.Extensions/Configuration/MetadataStructureValidator.cs
@@ -1,6 +1,7 @@
 ﻿using MFiles.VAF.Common;
 using MFiles.VAF.Configuration;
 using MFiles.VAF.Configuration.JsonAdaptor;
+using MFiles.VAF.Configuration.Logging;
 using MFiles.VAF.Configuration.Validation;
 using MFilesAPI;
 using System;
@@ -18,29 +19,67 @@ namespace MFiles.VAF.Extensions.Configuration
 	internal class MetadataStructureValidator
 		: VAF.Configuration.MetadataStructureValidator
 	{
+		/// <summary>
+		/// The logger for the metadata structure validator.
+		/// </summary>
+		private ILogger Logger { get; } = LogManager.GetLogger(typeof(MetadataStructureValidator));
+
+		/// <inheritdoc />
+		public override bool ValidateItem(Vault vault, IConfiguration configuration, object item, ValidationResultBase validationResult, Assembly[] containingAssemblies = null, int level = 10)
+		{
+			// Suppress validation exceptions if the configuration is null
+			// (which can happen if it fails deserialization).
+			if (item == null)
+			{
+				validationResult.ReportCustomFailure
+				(
+					configuration,
+					MFMetadataStructureItem.MFMetadataStructureItemNone,
+					"",
+					"The configuration was not provided; possible deserialization error (check configuration class structure).",
+					true
+				);
+				this.Logger?.Warn($"The provided configuration was null; possible deserialization error (check configuration class structure).");
+				return true;
+			}
+			return base.ValidateItem(vault, configuration, item, validationResult, containingAssemblies, level);
+		}
+
 		/// <inheritdoc />
 		protected virtual void ResolveOwnerOrDefaultPropertyDefs(Vault vault, object item)
 		{
 			// Sanity.
 			if (item == null)
 				return;
-			
+
 			// Find child properties/fields that might have the attributes we care about.
 			var children = this.GetChildren(item);
 			foreach ( var child in children )
 			{
-				// If it doesn't have the attribute we care about then skip.
-				var attr = child?.GetCustomAttribute(typeof(OwnerOrDefaultPropertyDefAttribute), true) as OwnerOrDefaultPropertyDefAttribute;
-				if (null == attr)
+				// Sanity.
+				if (null == child)
 					continue;
+				this.Logger?.Trace($"Checking {child.DeclaringType?.FullName}.{child.Name} for OwnerOrDefaultPropertyDefAttribute attributes.");
+
+				// If it doesn't have the attribute we care about then skip.
+				if (!(child?.GetCustomAttribute(typeof(OwnerOrDefaultPropertyDefAttribute), true) is OwnerOrDefaultPropertyDefAttribute attr))
+				{
+					this.Logger?.Trace($"No attribute found; skipping");
+					continue;
+				}
 
 				// Try to resolve the value.
+				this.Logger?.Debug($"{attr.GetType().Name} attribute found on {child.DeclaringType?.FullName}.{child.Name}.");
 				var identifier = attr.Resolve(vault, item.GetType(), item);
 				if (null == identifier)
+				{
+					this.Logger?.Info($"Could not resolve the object type associated with {child.DeclaringType?.FullName}.{child.Name} (looked for populated configuration property named {attr.ObjectTypeReference}).");
 					continue;
+				}
 
 				// Set the value.
 				{
+					this.Logger?.Debug($"Setting {child.DeclaringType?.FullName}.{child.Name} to {identifier.ID}.");
 					if (child.MemberType == MemberTypes.Field)
 						((FieldInfo)child).SetValue(item, identifier);
 					if (child.MemberType == MemberTypes.Property)
