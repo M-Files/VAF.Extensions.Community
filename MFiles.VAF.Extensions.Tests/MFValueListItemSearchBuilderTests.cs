7f3f9bff7245e0baf2ccd886c5018db843324c0e
Craig Hawker <craig.hawker@m-files.com>
2025-07-24 17:37:39 +0100

    Initial implementation of MFValueListSearchBuilder. Tests for above.

---
+++ MFiles.VAF.Extensions.Tests/MFValueListItemSearchBuilderTests.cs
@@ -0,0 +1,357 @@
+﻿using MFilesAPI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFiles.VAF.Extensions.Tests
+{
+	[TestClass]
+	public class MFValueListItemSearchBuilderTests
+	{
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void NullVaultThrows()
+		{
+			new MFValueListItemSearchBuilder(null, 1);
+		}
+
+		[TestMethod]
+		public void ValidVaultDoesNotThrow()
+		{
+			new MFValueListItemSearchBuilder(Mock.Of<Vault>(), 1);
+		}
+
+		[TestMethod]
+		public void ValueListIdSet()
+		{
+			Assert.AreEqual
+			(
+				123,
+				new MFValueListItemSearchBuilder(Mock.Of<Vault>(), 123).ValueListId
+			);
+		}
+
+		[TestMethod]
+		public void SearchConditionsEmptyByDefault()
+		{
+			Assert.AreEqual
+			(
+				0,
+				new MFValueListItemSearchBuilder(Mock.Of<Vault>(), 1).SearchConditions.Count
+			);
+		}
+
+		[TestMethod]
+		public void Name()
+		{
+			var searchBuilder = new MFValueListItemSearchBuilder(Mock.Of<Vault>(), 1);
+			var ret = searchBuilder.Name("hello world");
+			Assert.AreEqual(searchBuilder, ret);
+			var condition = searchBuilder.SearchConditions.Cast<SearchCondition>().FirstOrDefault();
+			Assert.IsNotNull(condition);
+			Assert.AreEqual(MFExpressionType.MFExpressionTypePropertyValue, condition.Expression.Type);
+			Assert.AreEqual((int)MFValueListItemPropertyDef.MFValueListItemPropertyDefName, condition.Expression.DataPropertyValuePropertyDef);
+			Assert.AreEqual("hello world", condition.TypedValue.Value);
+			Assert.AreEqual(MFDataType.MFDatatypeText, condition.TypedValue.DataType);
+			Assert.AreEqual(MFConditionType.MFConditionTypeEqual, condition.ConditionType);
+		}
+
+		[TestMethod]
+		public void Name_CustomConditionType()
+		{
+			var searchBuilder = new MFValueListItemSearchBuilder(Mock.Of<Vault>(), 1);
+			var ret = searchBuilder.Name("hello", MFConditionType.MFConditionTypeStartsWith);
+			var condition = searchBuilder.SearchConditions.Cast<SearchCondition>().FirstOrDefault();
+			Assert.IsNotNull(condition);
+			Assert.AreEqual(MFConditionType.MFConditionTypeStartsWith, condition.ConditionType);
+		}
+
+		[TestMethod]
+		public void Deleted()
+		{
+			var searchBuilder = new MFValueListItemSearchBuilder(Mock.Of<Vault>(), 1);
+			var ret = searchBuilder.Deleted(true);
+			Assert.AreEqual(searchBuilder, ret);
+			var condition = searchBuilder.SearchConditions.Cast<SearchCondition>().FirstOrDefault();
+			Assert.IsNotNull(condition);
+			Assert.AreEqual(MFExpressionType.MFExpressionTypePropertyValue, condition.Expression.Type);
+			Assert.AreEqual((int)MFValueListItemPropertyDef.MFValueListItemPropertyDefDeleted, condition.Expression.DataPropertyValuePropertyDef);
+			Assert.AreEqual(true, condition.TypedValue.Value);
+			Assert.AreEqual(MFDataType.MFDatatypeBoolean, condition.TypedValue.DataType);
+			Assert.AreEqual(MFConditionType.MFConditionTypeEqual, condition.ConditionType);
+		}
+
+		[TestMethod]
+		public void Deleted_CustomConditionType()
+		{
+			var searchBuilder = new MFValueListItemSearchBuilder(Mock.Of<Vault>(), 1);
+			var ret = searchBuilder.Deleted(true, MFConditionType.MFConditionTypeNotEqual);
+			var condition = searchBuilder.SearchConditions.Cast<SearchCondition>().FirstOrDefault();
+			Assert.IsNotNull(condition);
+			Assert.AreEqual(MFConditionType.MFConditionTypeNotEqual, condition.ConditionType);
+		}
+
+		[TestMethod]
+		public void Owner()
+		{
+			var searchBuilder = new MFValueListItemSearchBuilder(Mock.Of<Vault>(), 1);
+			var ret = searchBuilder.Owner(12345);
+			Assert.AreEqual(searchBuilder, ret);
+			var condition = searchBuilder.SearchConditions.Cast<SearchCondition>().FirstOrDefault();
+			Assert.IsNotNull(condition);
+			Assert.AreEqual(MFExpressionType.MFExpressionTypePropertyValue, condition.Expression.Type);
+			Assert.AreEqual((int)MFValueListItemPropertyDef.MFValueListItemPropertyDefOwner, condition.Expression.DataPropertyValuePropertyDef);
+			Assert.AreEqual(MFDataType.MFDatatypeLookup, condition.TypedValue.DataType);
+			Assert.AreEqual(12345, condition.TypedValue.GetLookupID());
+			Assert.AreEqual(MFConditionType.MFConditionTypeEqual, condition.ConditionType);
+		}
+
+		[TestMethod]
+		public void Owner_CustomConditionType()
+		{
+			var searchBuilder = new MFValueListItemSearchBuilder(Mock.Of<Vault>(), 1);
+			var ret = searchBuilder.Owner(12345, MFConditionType.MFConditionTypeNotEqual);
+			var condition = searchBuilder.SearchConditions.Cast<SearchCondition>().FirstOrDefault();
+			Assert.IsNotNull(condition);
+			Assert.AreEqual(MFConditionType.MFConditionTypeNotEqual, condition.ConditionType);
+		}
+
+		[TestMethod]
+		public void ExternalId()
+		{
+			var searchBuilder = new MFValueListItemSearchBuilder(Mock.Of<Vault>(), 1);
+			var ret = searchBuilder.ExternalId("extid-11");
+			Assert.AreEqual(searchBuilder, ret);
+			var condition = searchBuilder.SearchConditions.Cast<SearchCondition>().FirstOrDefault();
+			Assert.IsNotNull(condition);
+			Assert.AreEqual(MFExpressionType.MFExpressionTypePropertyValue, condition.Expression.Type);
+			Assert.AreEqual((int)MFValueListItemPropertyDef.MFValueListItemPropertyDefExtID, condition.Expression.DataPropertyValuePropertyDef);
+			Assert.AreEqual("extid-11", condition.TypedValue.Value);
+			Assert.AreEqual(MFDataType.MFDatatypeText, condition.TypedValue.DataType);
+			Assert.AreEqual(MFConditionType.MFConditionTypeEqual, condition.ConditionType);
+		}
+
+		[TestMethod]
+		public void ExternalId_CustomConditionType()
+		{
+			var searchBuilder = new MFValueListItemSearchBuilder(Mock.Of<Vault>(), 1);
+			var ret = searchBuilder.ExternalId("extid-11", MFConditionType.MFConditionTypeNotEqual);
+			var condition = searchBuilder.SearchConditions.Cast<SearchCondition>().FirstOrDefault();
+			Assert.IsNotNull(condition);
+			Assert.AreEqual(MFConditionType.MFConditionTypeNotEqual, condition.ConditionType);
+		}
+
+		[TestMethod]
+		public void Find()
+		{
+			// Set up our mock objects.
+			var valueListItemOperationsMock = new Moq.Mock<VaultValueListItemOperations>();
+			valueListItemOperationsMock
+				.Setup(m => m.SearchForValueListItemsEx2
+				(
+					It.IsAny<int>(),
+					It.IsAny<SearchConditions>(),
+					It.IsAny<bool>(),
+					It.IsAny<MFExternalDBRefreshType>(),
+					It.IsAny<bool>(),
+					It.IsAny<int>(),
+					It.IsAny<int>()
+				)).Callback((int vl, SearchConditions c, bool u, MFExternalDBRefreshType rt, bool rcu, int pid, int mr) =>
+				{
+					Assert.AreEqual(12345, vl);
+					Assert.AreEqual(1, c.Count);
+					Assert.AreEqual(false, u);
+					Assert.AreEqual(MFExternalDBRefreshType.MFExternalDBRefreshTypeNone, rt);
+					Assert.AreEqual(false, rcu);
+					Assert.AreEqual(-1, pid);
+					Assert.AreEqual(5000, mr);
+				}).Returns(Mock.Of<ValueListItemSearchResults>());
+			var vaultMock = new Moq.Mock<Vault>();
+			vaultMock
+				.SetupGet(m => m.ValueListItemOperations)
+				.Returns(valueListItemOperationsMock.Object);
+
+			// Instantiate the builder and set something to check for.
+			var searchBuilder = new MFValueListItemSearchBuilder(vaultMock.Object, 12345);
+			searchBuilder.ExternalId("extid-11");
+
+			// Find.
+			var results = searchBuilder.Find();
+
+			// Ensure that we hit the underlying method..
+			valueListItemOperationsMock
+				.Verify
+				(
+					m => m.SearchForValueListItemsEx2
+					(
+						It.IsAny<int>(),
+						It.IsAny<SearchConditions>(),
+						It.IsAny<bool>(),
+						It.IsAny<MFExternalDBRefreshType>(),
+						It.IsAny<bool>(),
+						It.IsAny<int>(),
+						It.IsAny<int>()
+					),
+					Times.Once()
+				);
+		}
+
+		public static IEnumerable<object[]> FindParameters()
+		{
+			// First couple are different, rest are defaults.
+			yield return new object[]
+			{
+					12345,
+					new SearchConditions(),
+					false,
+					MFExternalDBRefreshType.MFExternalDBRefreshTypeNone,
+					false,
+					-1,
+					5000
+			};
+
+			// Now let's vary some
+			yield return new object[]
+			{
+					12345,
+					new SearchConditions(),
+					true,
+					MFExternalDBRefreshType.MFExternalDBRefreshTypeNone,
+					false,
+					-1,
+					5000
+			};
+			yield return new object[]
+				{
+					12345,
+					new SearchConditions(),
+					false,
+					MFExternalDBRefreshType.MFExternalDBRefreshTypeFull,
+					false,
+					-1,
+					5000
+				};
+			yield return new object[]
+				{
+					1,
+					new SearchConditions(),
+					false,
+					MFExternalDBRefreshType.MFExternalDBRefreshTypeNone,
+					false,
+					-1,
+					5000
+				};
+			yield return new object[]
+				{
+					12345,
+					new SearchConditions(),
+					false,
+					MFExternalDBRefreshType.MFExternalDBRefreshTypeNone,
+					true,
+					-1,
+					5000
+				};
+			yield return new object[]
+				{
+					12345,
+					new SearchConditions(),
+					false,
+					MFExternalDBRefreshType.MFExternalDBRefreshTypeNone,
+					false,
+					123,
+					5000
+				};
+			yield return new object[]
+				{
+					12345,
+					new SearchConditions(),
+					false,
+					MFExternalDBRefreshType.MFExternalDBRefreshTypeNone,
+					false,
+					-1,
+					1000
+				};
+		}
+
+		[TestMethod]
+		[DynamicData(nameof(FindParameters), DynamicDataSourceType.Method)]
+		public void Find_CustomParameters
+		(
+			int valueListId,
+			SearchConditions searchConditions,
+			bool updateFromServer,
+			MFExternalDBRefreshType refreshType,
+			bool replaceCurrentUserWithCallersIdentity,
+			int propertyDefId,
+			int maximumResults
+		)
+		{
+			// Set up our mock objects.
+			var valueListItemOperationsMock = new Moq.Mock<VaultValueListItemOperations>();
+			valueListItemOperationsMock
+				.Setup(m => m.SearchForValueListItemsEx2
+				(
+					It.IsAny<int>(),
+					It.IsAny<SearchConditions>(),
+					It.IsAny<bool>(),
+					It.IsAny<MFExternalDBRefreshType>(),
+					It.IsAny<bool>(),
+					It.IsAny<int>(),
+					It.IsAny<int>()
+				)).Callback((int vl, SearchConditions c, bool u, MFExternalDBRefreshType rt, bool rcu, int pid, int mr) =>
+				{
+					Assert.AreEqual(valueListId, vl);
+					Assert.AreEqual(searchConditions.Count, c.Count); // TODO: Check the contents.
+					Assert.AreEqual(updateFromServer, u);
+					Assert.AreEqual(refreshType, rt);
+					Assert.AreEqual(replaceCurrentUserWithCallersIdentity, rcu);
+					Assert.AreEqual(propertyDefId, pid);
+					Assert.AreEqual(maximumResults, mr);
+				}).Returns(Mock.Of<ValueListItemSearchResults>());
+			var vaultMock = new Moq.Mock<Vault>();
+			vaultMock
+				.SetupGet(m => m.ValueListItemOperations)
+				.Returns(valueListItemOperationsMock.Object);
+
+			// Instantiate the builder.
+			var searchBuilder = new MFValueListItemSearchBuilder(vaultMock.Object, valueListId);
+			if(null != searchConditions)
+			{
+				foreach (var sc in searchConditions.Cast<SearchCondition>())
+					searchBuilder.SearchConditions.Add(0, sc);
+			}
+
+			// Find.
+			var results = searchBuilder.Find
+			(
+				updateFromServer,
+				refreshType,
+				replaceCurrentUserWithCallersIdentity,
+				propertyDefId,
+				maximumResults
+			);
+
+			// Ensure that we hit the underlying method..
+			valueListItemOperationsMock
+				.Verify
+				(
+					m => m.SearchForValueListItemsEx2
+					(
+						It.IsAny<int>(),
+						It.IsAny<SearchConditions>(),
+						It.IsAny<bool>(),
+						It.IsAny<MFExternalDBRefreshType>(),
+						It.IsAny<bool>(),
+						It.IsAny<int>(),
+						It.IsAny<int>()
+					),
+					Times.Once()
+				);
+		}
+
+	}
+}
---
+++ MFiles.VAF.Extensions/MFValueListSearchBuilder.cs
@@ -0,0 +1,190 @@
+﻿using MFilesAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFiles.VAF.Extensions
+{
+	/// <summary>
+	/// Enables searching for value list items, similar to
+	/// how <see cref="MFiles.VAF.Common.MFSearchBuilder"/> does for objects.
+	/// </summary>
+	public class MFValueListItemSearchBuilder
+	{
+		/// <summary>
+		/// The vault to use for searching.
+		/// </summary>
+		public Vault Vault { get; }
+
+		/// <summary>
+		/// The search conditions.
+		/// </summary>
+		public SearchConditions SearchConditions { get; }
+			= new SearchConditions();
+
+		/// <summary>
+		/// The value list ID to search.
+		/// </summary>
+		public int ValueListId { get; }
+
+		/// <summary>
+		/// Instantiates the search builder.
+		/// </summary>
+		/// <param name="vault">The vault reference to use for searching.</param>
+		/// <param name="valueListId">The value list to search.</param>
+		/// <exception cref="ArgumentNullException">If <paramref name="vault"/> is null.</exception>
+		public MFValueListItemSearchBuilder(Vault vault, int valueListId)
+		{
+			this.Vault = vault 
+				?? throw new ArgumentNullException(nameof(vault));
+			this.ValueListId = valueListId;
+		}
+
+		/// <summary>
+		/// Adds a "name" search condition.
+		/// </summary>
+		/// <param name="name">The name of the item to search for.</param>
+		/// <param name="conditionType">The type of comparison to do.</param>
+		/// <returns>The current <see cref="MFValueListItemSearchBuilder"/>.</returns>
+		public MFValueListItemSearchBuilder Name(string name, MFConditionType conditionType = MFConditionType.MFConditionTypeEqual)
+		{
+			// Create the condition.
+			var condition = new SearchCondition();
+
+			// Set the expression.
+			condition.Expression.SetValueListItemExpression(MFValueListItemPropertyDef.MFValueListItemPropertyDefName,
+				MFParentChildBehavior.MFParentChildBehaviorNone);
+
+			// Set the condition type.
+			condition.ConditionType = conditionType;
+
+			// Set the value.
+			condition.TypedValue.SetValue(MFDataType.MFDatatypeText, name);
+
+			// Add the condition.
+			this.SearchConditions.Add(0, condition);
+
+			// Return itself for chaining.
+			return this;
+		}
+
+		/// <summary>
+		/// Adds a "deleted" search condition.
+		/// </summary>
+		/// <param name="deletedItems">If <see langword="true"/> then includes only deleted items, if <see langword="false"/> then includes only non-deleted items.</param>
+		/// <param name="conditionType">The type of comparison to do.</param>
+		/// <returns>The current <see cref="MFValueListItemSearchBuilder"/>.</returns>
+		public MFValueListItemSearchBuilder Deleted(bool deletedItems, MFConditionType conditionType = MFConditionType.MFConditionTypeEqual)
+		{
+			// Create the condition.
+			var condition = new SearchCondition();
+
+			// Set the expression.
+			condition.Expression.SetValueListItemExpression(MFValueListItemPropertyDef.MFValueListItemPropertyDefDeleted,
+				MFParentChildBehavior.MFParentChildBehaviorNone);
+
+			// Set the condition type.
+			condition.ConditionType = conditionType;
+
+			// Set the value.
+			condition.TypedValue.SetValue(MFDataType.MFDatatypeBoolean, deletedItems);
+
+			// Add the condition.
+			this.SearchConditions.Add(0, condition);
+
+			// Return itself for chaining.
+			return this;
+		}
+
+		/// <summary>
+		/// Adds an "owner" search condition.
+		/// </summary>
+		/// <param name="ownerId">The id of the owner value list item.</param>
+		/// <param name="conditionType">The type of comparison to do.</param>
+		/// <returns>The current <see cref="MFValueListItemSearchBuilder"/>.</returns>
+		public MFValueListItemSearchBuilder Owner(int ownerId, MFConditionType conditionType = MFConditionType.MFConditionTypeEqual)
+		{
+			// Create the condition.
+			var condition = new SearchCondition();
+
+			// Set the expression.
+			condition.Expression.SetValueListItemExpression(MFValueListItemPropertyDef.MFValueListItemPropertyDefOwner,
+				MFParentChildBehavior.MFParentChildBehaviorNone);
+
+			// Set the condition type.
+			condition.ConditionType = conditionType;
+
+			// Set the value.
+			condition.TypedValue.SetValue(MFDataType.MFDatatypeLookup, ownerId);
+
+			// Add the condition.
+			this.SearchConditions.Add(0, condition);
+
+			// Return itself for chaining.
+			return this;
+		}
+
+		/// <summary>
+		/// Adds a condition that filters to only items that match a given external ID,
+		/// </summary>
+		/// <param name="externalId">The external ID.</param>
+		/// <param name="conditionType">The type of comparison to do.</param>
+		/// <returns>The current <see cref="MFValueListItemSearchBuilder"/>.</returns>
+		public MFValueListItemSearchBuilder ExternalId(string externalId, MFConditionType conditionType = MFConditionType.MFConditionTypeEqual)
+		{
+			// Create the condition.
+			var condition = new SearchCondition();
+
+			// Set the expression.
+			condition.Expression.SetValueListItemExpression(MFValueListItemPropertyDef.MFValueListItemPropertyDefExtID,
+				MFParentChildBehavior.MFParentChildBehaviorNone);
+
+			// Set the condition type.
+			condition.ConditionType = conditionType;
+
+			// Set the value.
+			condition.TypedValue.SetValue(MFDataType.MFDatatypeText, externalId);
+
+			// Add the condition.
+			this.SearchConditions.Add(0, condition);
+
+			// Return itself for chaining.
+			return this;
+		}
+
+		/// <summary>
+		/// Executes the search.
+		/// </summary>
+		/// <param name="updateFromServer">If this is set to "true", the search operation is always carried out on the server-side.</param>
+		/// <param name="refreshType">Specifies how, if at all, external value lists are updated for this operation.</param>
+		/// <param name="replaceCurrentUserWithCallersIdentity"></param>
+		/// <param name="propertyDefId">Specifies the property definition the filter of which should be applied to the list (-1 for none).</param>
+		/// <param name="maximumResults">The maximum number of results to return. By default, no more than 5,000 results are allowed.</param>
+		/// <returns>Any matching value list items.</returns>
+		/// <remarks>Uses <see cref="VaultValueListItemOperationsClass.SearchForValueListItemsEx2"/> to search.</remarks>
+		public ValueListItemSearchResults Find
+		(
+			bool updateFromServer = false,
+			MFExternalDBRefreshType refreshType = MFExternalDBRefreshType.MFExternalDBRefreshTypeNone,
+			bool replaceCurrentUserWithCallersIdentity = false,
+			int propertyDefId = -1,
+			int maximumResults = 5000
+		)
+		{
+			// Delegate.
+			return this.Vault.ValueListItemOperations.SearchForValueListItemsEx2
+			(
+				this.ValueListId,
+				this.SearchConditions,
+				updateFromServer,
+				refreshType,
+				replaceCurrentUserWithCallersIdentity,
+				propertyDefId,
+				maximumResults
+			);
+		}
+
+	}
+}
